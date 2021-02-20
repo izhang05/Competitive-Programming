285674f3b5d338e6718afa9f8ab24dc1b9272b46
Ian Zhang <ianzhang0510@gmail.com>
2021-02-19 20:45:41 -0500

    Solved CSES Stick Divisions

---
+++ CSES/X 11. Additional Problems/Y 65. Stick Divisions.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+mt19937 rng((uint32_t) chrono::steady_clock::now().time_since_epoch().count());
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<class T>
+using indexed_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<class T>
+using indexed_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<class T>
+void print(T a, string sep = " ", string end = "\n") {
+    for (auto i : a) {
+        cout << i << sep;
+    }
+    cout << end;
+}
+//#define DEBUG
+void setIO() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin.exceptions(istream::failbit);
+#ifdef LOCAL
+    freopen("65.in", "r", stdin);
+    freopen("65.out", "w", stdout);
+#endif
+}
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7;
+
+
+int main() {
+#ifdef LOCAL
+    auto start_time = std::chrono::high_resolution_clock::now();
+#endif
+    setIO();
+    int x, n;
+    cin >> x >> n;
+    long long sol = 0;
+    int a;
+    priority_queue<int, vector<int>, greater<>> nums;
+    for (int i = 0; i < n; ++i) {
+        cin >> a;
+        nums.push(a);
+    }
+    int b;
+    while (nums.size() >= 2) {
+        a = nums.top();
+        nums.pop();
+        b = nums.top();
+        nums.pop();
+        sol += a + b;
+        nums.push(a + b);
+    }
+    cout << sol << "\n";
+#ifdef LOCAL
+    auto end_time = std::chrono::high_resolution_clock::now();
+    cout << setprecision(6) << fixed;
+    cout << "Execution time: " << std::chrono::duration_cast<std::chrono::duration<double>>(end_time - start_time).count() << " seconds" << endl;
+#endif
+    return 0;
+}
