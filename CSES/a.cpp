5fcfce0783bbc7b9b36b2c12b44a0b6e6db12347
Ian Zhang <ianzhang0510@gmail.com>
2021-03-03 13:07:35 -0500

    Solved Codeforces Lomsat gelral https://codeforces.com/contest/600/problem/E

--- CSES/a.cpp
+++ CSES/a.cpp
@@ -29,11 +29,59 @@ void setIO() {
     freopen("1.out", "w", stderr);
 #endif
 }
-const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxp = 7, maxn = 1e5 + 5;
-long long dp[maxn][1 << maxp], a[maxn], s[maxn][maxp];
 
-bool cmp(int x, int y) {
-    return a[x] > a[y];
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 1e5 + 5;
+long long sol[maxn], c[maxn];
+unordered_map<long long, long long, custom_hash> col[maxn];
+map<long long, unordered_set<long long, custom_hash>, greater<>> cnt[maxn];
+map<long long, long long, greater<>> cnt_sum[maxn];
+vector<long long> adj[maxn];
+
+void dfs(int node, int p) {
+    //    if (col[node].find(c[node]) != col[node].end()) {
+    //        cnt[node][col[node][c[node]]].erase(c[node]);
+    //    }
+    ++col[node][c[node]];
+    if (cnt[node][col[node][c[node]]].find(c[node]) == cnt[node][col[node][c[node]]].end()) {
+        cnt[node][col[node][c[node]]].insert(c[node]);
+        cnt_sum[node][col[node][c[node]]] += c[node];
+    }
+    for (int i : adj[node]) {
+        if (i != p) {
+            dfs(i, node);
+            if (col[i].size() > col[node].size()) {
+                swap(col[i], col[node]);
+                swap(cnt[i], cnt[node]);
+                swap(cnt_sum[i], cnt_sum[node]);
+            }
+            for (pair<int, int> j : col[i]) {
+                //                if (col[node].find(j.first) != col[node].end()) {
+                //                    cnt[node][col[node][j.first]].erase(j.first);
+                //                }
+                col[node][j.first] += j.second;
+                if (cnt[node][col[node][j.first]].find(j.first) == cnt[node][col[node][j.first]].end()) {
+                    cnt[node][col[node][j.first]].insert(j.first);
+                    cnt_sum[node][col[node][j.first]] += j.first;
+                }
+            }
+        }
+    }
+    sol[node] = cnt_sum[node].begin()->second;
 }
 
 int main() {
@@ -41,55 +89,36 @@ int main() {
     auto start_time = chrono::high_resolution_clock::now();
 #endif
     setIO();
-    int n, p, k;
-    cin >> n >> p >> k;
-    vector<int> players(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> a[i];
-        players[i] = i;
-    }
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < p; ++j) {
-            cin >> s[i][j];
-        }
-    }
 
-    sort(players.begin(), players.end(), cmp);
-#ifdef DEBUG
-    cout << "players: ";
-    print(players);
-#endif
-    memset(dp, -1, sizeof(dp));
-    dp[0][0] = 0;
-    for (int i = 1; i <= n; ++i) {
-        for (int mask = 0; mask < (1 << p); ++mask) {
-            long long &cur = dp[i][mask];
-            if (dp[i - 1][mask] != -1) {
-                cur = dp[i - 1][mask];
-                int taken = i - __builtin_popcount(mask);
-                if (taken <= k) {
-                    cur = max(cur, dp[i - 1][mask] + a[players[i - 1]]);
-                }
-            }
-            for (int j = 0; j < p; ++j) {
-                if (mask & (1 << j) && dp[i - 1][mask ^ (1 << j)] != -1) {
-                    cur = max(cur, dp[i - 1][mask ^ (1 << j)] + s[players[i - 1]][j]);
-                }
-            }
-        }
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> c[i];
     }
-
+    for (int i = 0; i < n - 1; ++i) {
+        int a, b;
+        cin >> a >> b;
+        --a, --b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    dfs(0, -1);
+    for (int i = 0; i < n - 1; ++i) {
+        cout << sol[i] << " ";
+    }
+    cout << sol[n - 1] << "\n";
 #ifdef DEBUG
-    for (int i = 1; i < n + 1; ++i) {
-        for (int mask = 0; mask < (1 << p); ++mask) {
-            cout << dp[i][mask] << " ";
+    for (auto i : cnt[0]) {
+        cout << i.first << ": ";
+        for (int j : i.second) {
+            cout << j << " ";
         }
         cout << "\n";
     }
-    cout << "sol: ";
+    for (pair<int, int> i : cnt_sum[0]) {
+        cout << i.first << " " << i.second << "\n";
+    }
 #endif
-
-    cout << dp[n][(1 << p) - 1] << "\n";
 
 #ifdef LOCAL
     auto end_time = chrono::high_resolution_clock::now();
