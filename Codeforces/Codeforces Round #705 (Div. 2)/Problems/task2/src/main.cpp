f9f985a1dc03ce2fb7e9ef2464c2606e6b183299
Ian Zhang <ianzhang0510@gmail.com>
2021-03-06 11:52:54 -0500

    Auto Commit

--- Codeforces/Codeforces Round #705 (Div. 2)/Problems/task2/src/main.cpp
+++ Codeforces/Codeforces Round #705 (Div. 2)/Problems/task2/src/main.cpp
@@ -3,25 +3,6 @@
 using namespace std;
 
 mt19937 rng((uint32_t) chrono::steady_clock::now().time_since_epoch().count());
-const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
-
-#include <ext/pb_ds/assoc_container.hpp>
-
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-template<class T>
-using indexed_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-
-template<class T>
-using indexed_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
-
-template<class T>
-void print(T a) {
-    for (auto i : a) {
-        cout << i << " ";
-    }
-    cout << "\n";
-}
 
 //#define DEBUG
 const int inf = 0x3f3f3f3f, mod = 1e9 + 7;
@@ -29,6 +10,7 @@ const int inf = 0x3f3f3f3f, mod = 1e9 + 7;
 vector<int> digits(int x) {
     return {x / 10, x % 10};
 }
+
 int h, m;
 
 void add_hour(int &x) {
