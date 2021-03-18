4b395305e3e28162ba47afb2ed2ba51d0b4e09fe
Ian Zhang <ianzhang0510@gmail.com>
2021-03-17 22:16:46 -0400

    Solved Codeforces Round #677 (Div. 3) - F. Zero Remainder Sum - https://codeforces.com/contest/1433/problem/F

--- CSES/b.cpp
+++ CSES/b.cpp
@@ -14,155 +14,57 @@ void setIO() {
     freopen("b.out", "w", stderr);
 #endif
 }
-template<class T>
-void print(T a, string sep = " ", string end = "\n") {
-    for (auto i : a) {
-        cout << i << sep;
-    }
-    cout << end;
-}
-const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 2e5 + 5;
-vector<int> adj[maxn];
-bool uni[maxn];
-pair<int, int> end_p, diameter;
-stack<int> nodes;
-bool found = false;
-int dist[maxn], rt[maxn], people[maxn];
-
-void dfs(int c, int p, int d) {
-    end_p = max(end_p, {d, c});
-    for (auto i : adj[c]) {
-        if (i != p) {
-            dfs(i, c, d + 1);
-        }
-    }
-}
-
-void dfs1(int c, int p) {
-    nodes.push(c);
-    if (c == diameter.second) {
-        found = true;
-        return;
-    }
-    for (auto i : adj[c]) {
-        if (i != p) {
-            dfs1(i, c);
-            if (found) {
-                return;
-            }
-        }
-    }
-    nodes.pop();
-}
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 72;
+int grid[maxn][maxn], dp[maxn][maxn][maxn][maxn];
 
 int main() {
     setIO();
 
-    int n, k;
-    cin >> n >> k;
-    vector<int> unis;
-    for (int i = 0; i < 2 * k; ++i) {
-        int a;
-        cin >> a;
-        --a;
-        uni[a] = true;
-        unis.push_back(a);
-    }
-    for (int i = 0; i < n - 1; ++i) {
-        int a, b;
-        cin >> a >> b;
-        --a, --b;
-        adj[a].push_back(b);
-        adj[b].push_back(a);
-    }
-    if (k == 1) {
-        memset(dist, 0x3f, sizeof(dist));
-        dist[unis[0]] = 0;
-        queue<int> q;
-        q.push(unis[0]);
-        while (!q.empty()) {
-            int cur = q.front();
-            q.pop();
-            for (auto i : adj[cur]) {
-                if (dist[i] == inf) {
-                    q.push(i);
-                    dist[i] = dist[cur] + 1;
+    int n, m, k;
+    cin >> n >> m >> k;
+    for (auto &i : dp) {
+        for (auto &j : i) {
+            for (auto &l : j) {
+                for (int &i1 : l) {
+                    i1 = -inf;
                 }
             }
         }
-        cout << dist[unis[1]] << "\n";
-        return 0;
     }
-    dfs(0, -1, 0);
-    diameter.first = end_p.second;
-    end_p = {0, 0};
-    dfs(diameter.first, -1, 0);
-    diameter.second = end_p.second;
-#ifdef DEBUG
-    cout << "diameter: "
-         << "\n";
-    cout << diameter.first << " " << diameter.second << "\n";
-#endif
-    dfs1(diameter.first, -1);
-    vector<int> path;
-    queue<pair<int, int>> q;
-    memset(dist, 0x3f, sizeof(dist));
-    while (!nodes.empty()) {
-        int cur = nodes.top();
-        path.push_back(cur);
-        q.push({cur, cur});
-        rt[cur] = cur;
-        dist[cur] = 0;
-        nodes.pop();
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cin >> grid[i][j];
+            dp[i][j][1][grid[i][j] % k] = grid[i][j];
+        }
     }
-    reverse(path.begin(), path.end());
-    while (!q.empty()) {
-        int cur = q.front().first, r = q.front().second;
-        q.pop();
-        for (auto i : adj[cur]) {
-            if (dist[i] == inf) {
-                rt[i] = r;
-                q.push({i, r});
-                dist[i] = dist[cur] + 1;
+    for (int x = 0; x < n; ++x) {
+        for (int y = 0; y < m; ++y) {
+            for (int take = 0; take < m; ++take) {
+                for (int rem = 0; rem < k; ++rem) {
+                    int cur = dp[x][y][take][rem];
+                    if (y != m - 1) {
+                        dp[x][y + 1][take][rem] = max(dp[x][y + 1][take][rem], cur);
+                        if (take < m / 2) {
+                            dp[x][y + 1][take + 1][(rem + grid[x][y + 1]) % k] = max(dp[x][y + 1][take + 1][(rem + grid[x][y + 1]) % k], cur + grid[x][y + 1]);
+                        }
+                    } else {
+                        if (x != n - 1) {
+                            dp[x + 1][0][0][rem] = max(dp[x + 1][0][0][rem], cur);
+                            dp[x + 1][0][1][(rem + grid[x + 1][0]) % k] = max(dp[x + 1][0][1][(rem + grid[x + 1][0]) % k], cur + grid[x + 1][0]);
+                        }
+                    }
+                }
             }
         }
     }
-    long long sol = 0;
+    int sol = 0;
     for (int i = 0; i < n; ++i) {
-        if (uni[i]) {
-            sol += dist[i];
-            ++people[rt[i]];
-        }
-    }
+        for (int j = 0; j < m; ++j) {
+            for (int l = 0; l < m; ++l) {
 #ifdef DEBUG
-    cout << "dist:"
-         << "\n";
-    for (int i = 0; i < n; ++i) {
-        cout << dist[i] << " ";
-    }
-    cout << "\n";
-    cout << "people:"
-         << "\n";
-    for (int i = 0; i < n; ++i) {
-        cout << people[i] << " ";
-    }
-    cout << "\n";
+                cout << dp[i][j][l][0] << "\n";
 #endif
-    int cur = 0;
-    bool off = false;
-    for (auto i : path) {
-        sol += cur;
-        if (off) {
-            cur -= people[i];
-        } else {
-            if (cur + people[i] <= k) {
-                cur += people[i];
-            } else {
-                cur = k - (people[i] - (k - cur));
-                off = true;
-            }
-            if (cur == k) {
-                off = true;
+                sol = max(sol, dp[i][j][l][0]);
             }
         }
     }
