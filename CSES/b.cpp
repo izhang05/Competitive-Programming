dd94a6e95066d1e154187bd8a9edd4fc6221610c
Ian Zhang <ianzhang0510@gmail.com>
2021-03-19 14:58:14 -0400

    Solved Codeforces Round #321 (Div. 2) - D. Kefa and Dishes - https://codeforces.com/contest/580/problem/D

--- CSES/b.cpp
+++ CSES/b.cpp
@@ -14,24 +14,54 @@ void setIO() {
     freopen("b.out", "w", stderr);
 #endif
 }
-const int inf = 0x3f3f3f3f, mod = 1e9 + 7;
+const int mod = 1e9 + 7, maxn = 19;
+long long dp[1 << maxn][maxn];
+const long long inf = 1e18;
 
 int main() {
     setIO();
-
-    int n;
-    cin >> n;
-    set<int> seen;
-    seen.insert(0);
-    for (int i = 1; i < n + 1; ++i) {
-        int a;
-        cin >> a;
-        auto it = seen.find(a);
-        if (it != seen.end()) {
-            seen.erase(it);
+    int n, m, k;
+    cin >> n >> m >> k;
+    vector<int> a(n);
+    for (auto &i : dp) {
+        for (long long &j : i) {
+            j = -inf;
         }
-        seen.insert(i);
     }
-    cout << seen.size() << "\n";
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+        dp[1 << i][i] = a[i];
+    }
+    map<int, map<int, int>> rules;
+    for (int i = 0; i < k; ++i) {
+        int b, c, d;
+        cin >> b >> c >> d;
+        --b, --c;
+        rules[b][c] = d;
+    }
+    long long sol = 0;
+    for (int mask = 1; mask < (1 << n); ++mask) {
+        for (int i = 0; i < n; ++i) {
+#ifdef DEBUG
+            cout << __builtin_popcount(mask) << "\n";
+            cout << mask << " " << i << "\n";
+            cout << dp[mask][i] << "\n";
+            cout << "\n";
+#endif
+            if (dp[mask][i] == -inf) {
+                continue;
+            }
+            if (__builtin_popcount(mask) == m) {
+                sol = max(sol, dp[mask][i]);
+            }
+            for (int j = 0; j < n; ++j) {
+                if (mask & (1 << j)) {
+                    continue;
+                }
+                dp[mask | (1 << j)][j] = max(dp[mask | (1 << j)][j], dp[mask][i] + a[j] + rules[i][j]);
+            }
+        }
+    }
+    cout << sol << "\n";
     return 0;
 }
