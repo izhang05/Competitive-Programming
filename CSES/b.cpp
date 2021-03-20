164cb7927b009401bee061b9670d7edfe3d8430e
Ian Zhang <ianzhang0510@gmail.com>
2021-03-20 18:59:59 -0400

    Solved Codeforces Round #540 (Div. 3) - F1. Tree Cutting (Easy Version) - https://codeforces.com/contest/1118/problem/F1

--- CSES/b.cpp
+++ CSES/b.cpp
@@ -14,95 +14,46 @@ void setIO(string name) {
     freopen((name + ".out").c_str(), "w", stderr);
 #endif
 }
-const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 1e5 + 5, maxs = 20;
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 3e5 + 5;
 vector<int> adj[maxn];
+long long color[maxn], sol = 0, tred, tblue, red[maxn], blue[maxn];
 
-int depth[maxn], sub[maxn], n, par[maxn], up[maxn][maxs], best[maxn], sol[maxn];
-bool visited[maxn];
-
-int jmp(int x, int d) {
-    for (int i = 0; i < maxs; i++) {
-        if ((d >> i) & 1) {
-            x = up[x][i];
-        }
+void dfs(int c, int p) {
+    if (color[c] == 1) {
+        red[c] = 1;
+    } else if (color[c] == 2) {
+        blue[c] = 1;
     }
-    return x;
-}
-
-int lca(int x, int y) {
-    if (depth[x] < depth[y]) {
-        swap(x, y);
-    }
-    x = jmp(x, depth[x] - depth[y]);
-    if (x == y) {
-        return x;
-    }
-    for (int i = maxs - 1; i >= 0; --i) {
-        int new_x = up[x][i], new_y = up[y][i];
-        if (new_x != new_y) {
-            x = new_x, y = new_y;
-        }
-    }
-    return up[x][0];
-}
-
-int dist(int x, int y) {
-    int l = lca(x, y);
-    return depth[x] - depth[l] + depth[y] - depth[l];
-}
-
-void dfs(int c, int p, int d) {
-    up[c][0] = p;
-    depth[c] = d;
     for (auto i : adj[c]) {
         if (i != p) {
-            dfs(i, c, d + 1);
+            dfs(i, c);
+            red[c] += red[i];
+            blue[c] += blue[i];
         }
     }
-}
-
-int find_centroid(int c, int p, int s) {
-    for (auto i : adj[c]) {
-        if (!visited[i] && sub[i] > s / 2 && i != p) {
-            return find_centroid(i, c, s);
-        }
-    }
-    return c;
-}
-
-int find_size(int v, int p = -1) {
-    if (visited[v]) {
-        return 0;
-    }
-    sub[v] = 1;
-
-    for (int x : adj[v]) {
-        if (x != p) {
-            sub[v] += find_size(x, v);
-        }
-    }
-
-    return sub[v];
-}
-
-void build(int v = 0, int p = -1, int d = 0) {
-    find_size(v);
-
-    int c = find_centroid(v, -1, sub[v]);
-    sol[c] = d;
-    visited[c] = true;
-    par[c] = p;
-
-    for (int x : adj[c]) {
-        if (!visited[x]) {
-            build(x, c, d + 1);
+    if ((red[c] == 0 || red[c] == tred) && (blue[c] == 0 || blue[c] == tblue)) {
+        if (!((red[c] == 0 && blue[c] == 0) || (red[c] == tred && blue[c] == tblue))) {
+#ifdef DEBUG
+            cout << c << "\n";
+#endif
+            ++sol;
         }
     }
 }
 
 int main() {
     setIO("b");
+
+    int n;
     cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> color[i];
+        if (color[i] == 1) {
+            ++tred;
+        } else if (color[i] == 2) {
+            ++tblue;
+        }
+    }
     for (int i = 0; i < n - 1; ++i) {
         int a, b;
         cin >> a >> b;
@@ -110,20 +61,7 @@ int main() {
         adj[a].push_back(b);
         adj[b].push_back(a);
     }
-    dfs(0, -1, 0);
-    for (int i = 1; i < maxs; ++i) {
-        for (int j = 0; j < n; ++j) {
-            if (up[j][i - 1] == -1) {
-                up[j][i] = -1;
-            } else {
-                up[j][i] = up[up[j][i - 1]][i - 1];
-            }
-        }
-    }
-    build();
-    for (int i = 0; i < n; ++i) {
-        cout << char(sol[i] + 65) << " ";
-    }
-    cout << "\n";
+    dfs(0, -1);
+    cout << sol << "\n";
     return 0;
 }
