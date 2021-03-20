0a2086dad6d2633d477ec0bb91c2942734a80eee
Ian Zhang <ianzhang0510@gmail.com>
2021-03-20 18:18:03 -0400

    Solved Codeforces Round #190 (Div. 1) - C. Ciel the Commander - https://codeforces.com/contest/321/problem/C

--- CSES/b.cpp
+++ CSES/b.cpp
@@ -4,64 +4,126 @@ using namespace std;
 
 mt19937 rng((uint32_t) chrono::steady_clock::now().time_since_epoch().count());
 //#define DEBUG
-void setIO() {
+void setIO(string name) {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cin.exceptions(istream::failbit);
 #ifdef LOCAL
-    freopen("b.in", "r", stdin);
-    freopen("b.out", "w", stdout);
-    freopen("b.out", "w", stderr);
+    freopen((name + ".in").c_str(), "r", stdin);
+    freopen((name + ".out").c_str(), "w", stdout);
+    freopen((name + ".out").c_str(), "w", stderr);
 #endif
 }
-const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 205, maxk = 1e3 + 5;
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 1e5 + 5, maxs = 20;
+vector<int> adj[maxn];
 
-long long dp[2][maxn / 2][maxk];
+int depth[maxn], sub[maxn], n, par[maxn], up[maxn][maxs], best[maxn], sol[maxn];
+bool visited[maxn];
+
+int jmp(int x, int d) {
+    for (int i = 0; i < maxs; i++) {
+        if ((d >> i) & 1) {
+            x = up[x][i];
+        }
+    }
+    return x;
+}
+
+int lca(int x, int y) {
+    if (depth[x] < depth[y]) {
+        swap(x, y);
+    }
+    x = jmp(x, depth[x] - depth[y]);
+    if (x == y) {
+        return x;
+    }
+    for (int i = maxs - 1; i >= 0; --i) {
+        int new_x = up[x][i], new_y = up[y][i];
+        if (new_x != new_y) {
+            x = new_x, y = new_y;
+        }
+    }
+    return up[x][0];
+}
+
+int dist(int x, int y) {
+    int l = lca(x, y);
+    return depth[x] - depth[l] + depth[y] - depth[l];
+}
+
+void dfs(int c, int p, int d) {
+    up[c][0] = p;
+    depth[c] = d;
+    for (auto i : adj[c]) {
+        if (i != p) {
+            dfs(i, c, d + 1);
+        }
+    }
+}
+
+int find_centroid(int c, int p, int s) {
+    for (auto i : adj[c]) {
+        if (!visited[i] && sub[i] > s / 2 && i != p) {
+            return find_centroid(i, c, s);
+        }
+    }
+    return c;
+}
+
+int find_size(int v, int p = -1) {
+    if (visited[v]) {
+        return 0;
+    }
+    sub[v] = 1;
+
+    for (int x : adj[v]) {
+        if (x != p) {
+            sub[v] += find_size(x, v);
+        }
+    }
+
+    return sub[v];
+}
+
+void build(int v = 0, int p = -1, int d = 0) {
+    find_size(v);
+
+    int c = find_centroid(v, -1, sub[v]);
+    sol[c] = d;
+    visited[c] = true;
+    par[c] = p;
+
+    for (int x : adj[c]) {
+        if (!visited[x]) {
+            build(x, c, d + 1);
+        }
+    }
+}
 
 int main() {
-    setIO();
-
-    int n, k;
-    cin >> n >> k;
-    vector<int> a(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> a[i];
-    }
-    sort(a.begin(), a.end());
-    dp[0][0][0] = 1;
-    dp[0][1][0] = 1;
+    setIO("b");
+    cin >> n;
     for (int i = 0; i < n - 1; ++i) {
-        for (int j = 0; j <= n / 2; ++j) {
-            for (int l = 0; l <= k; ++l) {
-                dp[i % 2][j][l] %= mod;
-                long long cur = dp[i % 2][j][l];
-#ifdef DEBUG
-                cout << i << " " << j << " " << l << " " << cur << "\n";
-#endif
-                if (cur == 0) {
-                    continue;
-                }
-                int val = l + j * (a[i + 1] - a[i]);
-                if (val >= maxk) {
-                    continue;
-                }
-                dp[(i + 1) % 2][j][val] += (j + 1) * cur;
-                dp[(i + 1) % 2][j][val] %= mod;
-                dp[(i + 1) % 2][j + 1][val] += cur;
-                dp[(i + 1) % 2][j + 1][val] %= mod;
-                if (j - 1 >= 0) {
-                    dp[(i + 1) % 2][j - 1][val] += j * cur;
-                    dp[(i + 1) % 2][j - 1][val] %= mod;
-                }
+        int a, b;
+        cin >> a >> b;
+        --a, --b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    dfs(0, -1, 0);
+    for (int i = 1; i < maxs; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (up[j][i - 1] == -1) {
+                up[j][i] = -1;
+            } else {
+                up[j][i] = up[up[j][i - 1]][i - 1];
             }
         }
-        memset(dp[i % 2], 0, sizeof(dp[i % 2]));
     }
-    long long sol = 0;
-    for (int i = 0; i <= k; ++i) {
-        sol += dp[(n - 1) % 2][0][i];
-        sol %= mod;
+    build();
+    for (int i = 0; i < n; ++i) {
+        cout << char(sol[i] + 65) << " ";
     }
-    cout << sol << "\n";
+    cout << "\n";
     return 0;
 }
