238d0403db535ab51d3d9be422e6b1a9c822b1ed
Ian Zhang <ianzhang0510@gmail.com>
2021-03-24 12:37:16 -0400

    Solved Tinkoff Challenge - Elimination Round - B. Igor and his way to work - https://codeforces.com/contest/793/problem/B

--- CSES/a.cpp
+++ CSES/a.cpp
@@ -14,45 +14,67 @@ void setIO(string name) {
     freopen((name + ".out").c_str(), "w", stderr);
 #endif
 }
-const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 5e3 + 5;
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 1e3 + 5;
+int dist[maxn][maxn][4], n, m;
+char grid[maxn][maxn];
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
 
-int dp[maxn][maxn];
+bool valid(int x, int y) {
+    return x >= 0 && y >= 0 && x < n && y < m && grid[x][y] != '*';
+}
 
 int main() {
     setIO("1");
-    int n, k;
-    cin >> n >> k;
-    vector<int> a(n);
+    cin >> n >> m;
+    memset(dist, 0x3f, sizeof(dist));
+    pair<int, int> dest;
+    queue<pair<pair<int, int>, int>> q;
     for (int i = 0; i < n; ++i) {
-        cin >> a[i];
-    }
-    sort(a.begin(), a.end());
-    for (int i = 0; i < n + 1; ++i) {
-        for (int j = 0; j < k + 1; ++j) {
-            int cur = dp[i][j];
-#ifdef DEBUG
-            cout << i << " " << j << " " << cur << "\n";
-#endif
-            dp[i + 1][j] = max(dp[i + 1][j], cur);
-            if (j == k) {
-                continue;
+        for (int j = 0; j < m; ++j) {
+            cin >> grid[i][j];
+            if (grid[i][j] == 'S') {
+                dest = {i, j};
+            } else if (grid[i][j] == 'T') {
+                dist[i][j][0] = dist[i][j][1] = dist[i][j][2] = dist[i][j][3] = 0;
+                q.push({{i, j}, 0});
+                q.push({{i, j}, 1});
+                q.push({{i, j}, 2});
+                q.push({{i, j}, 3});
             }
-            int lo = i + 1, hi = n - 1, mid, res = i;
-            while (lo <= hi) {
-                mid = (lo + hi) / 2;
-                if (a[mid] <= a[i] + 5) {
-                    res = mid;
-                    lo = mid + 1;
-                } else {
-                    hi = mid - 1;
-                }
-            }
-            //#ifdef DEBUG
-            //            cout << i << " " << res << "\n";
-            //#endif
-            dp[res + 1][j + 1] = max(dp[res + 1][j + 1], cur + res - i + 1);
         }
     }
-    cout << dp[n][k] << "\n";
+    while (!q.empty()) {
+        pair<pair<int, int>, int> cur = q.front();
+        q.pop();
+        int cur_dist = dist[cur.first.first][cur.first.second][cur.second];
+        for (int i = 0; i < 4; ++i) {
+            int nx = cur.first.first + dx[i], ny = cur.first.second + dy[i];
+            if (valid(nx, ny)) {
+                if (i == cur.second) {
+                    if (dist[nx][ny][cur.second] > cur_dist) {
+                        dist[nx][ny][cur.second] = cur_dist;
+                        q.push({{nx, ny}, cur.second});
+                    }
+                } else if (dist[nx][ny][i] > cur_dist + 1) {
+                    dist[nx][ny][i] = cur_dist + 1;
+                    q.push({{nx, ny}, i});
+                }
+            }
+        }
+    }
+    int sol = inf;
+    for (int i = 0; i < 4; ++i) {
+        sol = min(sol, dist[dest.first][dest.second][i]);
+    }
+#ifdef DEBUG
+    cout << sol << "\n";
+#endif
+    if (sol <= 2) {
+        cout << "YES"
+             << "\n";
+    } else {
+        cout << "NO"
+             << "\n";
+    }
     return 0;
 }
