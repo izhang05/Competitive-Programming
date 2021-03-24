8021c1749bbcb52cc3c6d8b47059529832a266bd
Ian Zhang <ianzhang0510@gmail.com>
2021-03-24 11:22:25 -0400

    Solved Codeforces Round #544 (Div. 3) - E. K Balanced Teams - https://codeforces.com/contest/1133/problem/E

--- CSES/a.cpp
+++ CSES/a.cpp
@@ -14,35 +14,45 @@ void setIO(string name) {
     freopen((name + ".out").c_str(), "w", stderr);
 #endif
 }
-const int inf = 0x3f3f3f3f, mod = 998244353, maxn = 2e5 + 5;
-long long p[maxn];
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 5e3 + 5;
+
+int dp[maxn][maxn];
 
 int main() {
     setIO("1");
-
-    int n, m;
-    cin >> n >> m;
-    string a, b;
-    cin >> a >> b;
-    reverse(a.begin(), a.end());
-    reverse(b.begin(), b.end());
-    p[0] = 1;
-    for (int i = 0; i < maxn - 1; ++i) {
-        p[i + 1] = (p[i] * 2) % mod;
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
     }
-    long long sol = 0, cnt = 0;
-    for (int i = m - 1; i >= 0; --i) {
-        if (b[i] == '1') {
-            ++cnt;
-        }
-        if (i < n && a[i] == '1') {
+    sort(a.begin(), a.end());
+    for (int i = 0; i < n + 1; ++i) {
+        for (int j = 0; j < k + 1; ++j) {
+            int cur = dp[i][j];
 #ifdef DEBUG
-            cout << i << " " << p[i] * cnt << "\n";
+            cout << i << " " << j << " " << cur << "\n";
 #endif
-            sol += p[i] * cnt;
+            dp[i + 1][j] = max(dp[i + 1][j], cur);
+            if (j == k) {
+                continue;
+            }
+            int lo = i + 1, hi = n - 1, mid, res = i;
+            while (lo <= hi) {
+                mid = (lo + hi) / 2;
+                if (a[mid] <= a[i] + 5) {
+                    res = mid;
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
+            //#ifdef DEBUG
+            //            cout << i << " " << res << "\n";
+            //#endif
+            dp[res + 1][j + 1] = max(dp[res + 1][j + 1], cur + res - i + 1);
         }
-        sol %= mod;
     }
-    cout << sol << "\n";
+    cout << dp[n][k] << "\n";
     return 0;
 }
