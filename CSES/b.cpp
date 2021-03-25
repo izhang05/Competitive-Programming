75cc3f6ad250b9220e46c38fbbc0233f509d97a2
Ian Zhang <ianzhang0510@gmail.com>
2021-03-25 19:06:01 -0400

    Solved Codeforces Round #338 (Div. 2) - B. Longtail Hedgehog - https://codeforces.com/contest/615/problem/B

--- CSES/b.cpp
+++ CSES/b.cpp
@@ -15,87 +15,32 @@ void setIO(string name) {
 #endif
 }
 const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 1e5 + 5;
+
+int dp[maxn];
 vector<int> adj[maxn];
-int p[maxn], h[maxn], total[maxn], u[maxn];
-bool sol = true;
-
-void dfs(int c, int par) {
-    total[c] = p[c];
-    for (auto i : adj[c]) {
-        if (i != par) {
-            dfs(i, c);
-            total[c] += total[i];
-            if (!sol) {
-                return;
-            }
-        }
-    }
-    if (total[c] % 2 != abs(h[c]) % 2 || abs(h[c]) > total[c]) {
-        sol = false;
-#ifdef DEBUG
-        cout << "bad: " << c << "\n";
-#endif
-        return;
-    }
-    u[c] = (total[c] - h[c]) / 2;
-}
-
-void dfs1(int c, int par) {
-    int sum = 0;
-    for (auto i : adj[c]) {
-        if (i != par) {
-            dfs(i, c);
-            if (!sol) {
-                return;
-            }
-            sum += u[i];
-        }
-    }
-    if (sum < u[c] - p[c]) {
-#ifdef DEBUG
-        cout << "bad: " << c << "\n";
-        cout << sum << " " << u[c] - p[c] << "\n";
-#endif
-        sol = false;
-        return;
-    }
-}
 
 int main() {
     setIO("b");
-    int t;
-    cin >> t;
-    while (t--) {
-        sol = true;
-        memset(p, 0, sizeof(p));
-        memset(h, 0, sizeof(h));
-        int n, m;
-        cin >> n >> m;
-        for (int i = 0; i < n; ++i) {
-            adj[i].clear();
-            cin >> p[i];
-        }
-        for (int i = 0; i < n; ++i) {
-            cin >> h[i];
-        }
-        for (int i = 0; i < n - 1; ++i) {
-            int a, b;
-            cin >> a >> b;
-            --a, --b;
-            adj[a].push_back(b);
-            adj[b].push_back(a);
-        }
-        dfs(0, -1);
-#ifdef DEBUG
-        cout << "total:"
-             << "\n";
-        for (int i = 0; i < n; ++i) {
-            cout << total[i] << " ";
-        }
-        cout << "\n";
-#endif
-        dfs1(0, -1);
-        cout << (sol ? "YES" : "NO") << "\n";
+
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < m; ++i) {
+        int a, b;
+        cin >> a >> b;
+        --a, --b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
     }
+    long long sol = 0;
+    for (int i = 0; i < n; ++i) {
+        dp[i] = 1;
+        for (auto j : adj[i]) {
+            if (j < i) {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+        sol = max(sol, (long long) (dp[i] * 1ll * adj[i].size()));
+    }
+    cout << sol << "\n";
     return 0;
 }
