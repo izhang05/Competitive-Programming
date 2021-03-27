88030e1e830a1c8feb2445d8e53f52b814a782ef
Ian Zhang <ianzhang0510@gmail.com>
2021-03-26 23:01:41 -0400

    Solved Educational Codeforces Round 39 (Rated for Div. 2) - D. Timetable - https://codeforces.com/contest/946/problem/D

--- CSES/b.cpp
+++ CSES/b.cpp
@@ -14,32 +14,57 @@ void setIO(string name) {
     freopen((name + ".out").c_str(), "w", stderr);
 #endif
 }
-const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 1e5 + 5;
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 505;
 
-int dp[maxn];
-vector<int> adj[maxn];
+int dp[maxn][maxn], mn[maxn][maxn];
+vector<int> classes[maxn];
 
 int main() {
     setIO("b");
-
-    int n, m;
-    cin >> n >> m;
-    for (int i = 0; i < m; ++i) {
-        int a, b;
-        cin >> a >> b;
-        --a, --b;
-        adj[a].push_back(b);
-        adj[b].push_back(a);
-    }
-    long long sol = 0;
+    int n, m, k;
+    cin >> n >> m >> k;
     for (int i = 0; i < n; ++i) {
-        dp[i] = 1;
-        for (auto j : adj[i]) {
-            if (j < i) {
-                dp[i] = max(dp[i], dp[j] + 1);
+        string s;
+        cin >> s;
+        for (int j = 0; j < m; ++j) {
+            if (s[j] == '1') {
+                classes[i].push_back(j);
             }
         }
-        sol = max(sol, (long long) (dp[i] * 1ll * adj[i].size()));
+    }
+    memset(mn, 0x3f, sizeof(mn));
+    for (int i = 0; i < n; ++i) {
+        mn[i][0] = 0;
+        for (int j = 0; j < (int) classes[i].size(); ++j) {
+            for (int l = j; l < (int) classes[i].size(); ++l) {
+                mn[i][l - j + 1] = min(mn[i][l - j + 1], classes[i][l] - classes[i][j] + 1);
+            }
+        }
+#ifdef DEBUG
+        for (int j = 0; j <= (int) classes[i].size(); ++j) {
+            cout << mn[i][j] << " ";
+        }
+        cout << "\n";
+#endif
+    }
+    memset(dp, 0x3f, sizeof(dp));
+    for (int i = 0; i < (int) classes[0].size() + 1; ++i) {
+        dp[0][i] = mn[0][classes[0].size() - i];
+    }
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j <= k; ++j) {
+            int cur = dp[i][j];
+            if (cur == inf) {
+                continue;
+            }
+            for (int skip = 0; skip <= (int) classes[i + 1].size() && j + skip <= k; ++skip) {
+                dp[i + 1][j + skip] = min(dp[i + 1][j + skip], cur + mn[i + 1][(int) classes[i + 1].size() - skip]);
+            }
+        }
+    }
+    int sol = inf;
+    for (int i = 0; i <= k; ++i) {
+        sol = min(sol, dp[n - 1][i]);
     }
     cout << sol << "\n";
     return 0;
