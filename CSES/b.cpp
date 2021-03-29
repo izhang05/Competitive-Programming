e9618f30675f20b145a0a1cd8c424f4b22614fe7
Ian Zhang <ianzhang0510@gmail.com>
2021-03-29 12:40:20 -0400

    Solved Codeforces Round #135 (Div. 2) - D. Choosing Capital for Treeland - https://codeforces.com/contest/219/problem/D

--- CSES/b.cpp
+++ CSES/b.cpp
@@ -14,58 +14,82 @@ void setIO(string name) {
     freopen((name + ".out").c_str(), "w", stderr);
 #endif
 }
-const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 505;
+template<class T>
+void print(T a, string sep = " ", string end = "\n") {
+    for (auto i : a) {
+        cout << i << sep;
+    }
+    cout << end;
+}
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 2e5 + 5;
+vector<pair<int, int>> adj[maxn];
+int down[maxn], up[maxn];
 
-int dp[maxn][maxn], mn[maxn][maxn];
-vector<int> classes[maxn];
+void dfs1(int c, int p) {
+    for (auto i : adj[c]) {
+        if (i.first != p) {
+            down[c] += i.second;
+            dfs1(i.first, c);
+            down[c] += down[i.first];
+        }
+    }
+}
+
+void dfs2(int c, int p, int cost) {
+    if (c != 0) {
+        up[c] = up[p] + down[p] - down[c];
+        if (cost == 1) {
+            --up[c];
+        } else {
+            ++up[c];
+        }
+    }
+    for (auto i : adj[c]) {
+        if (i.first != p) {
+            dfs2(i.first, c, i.second);
+        }
+    }
+}
 
 int main() {
     setIO("b");
-    int n, m, k;
-    cin >> n >> m >> k;
-    for (int i = 0; i < n; ++i) {
-        string s;
-        cin >> s;
-        for (int j = 0; j < m; ++j) {
-            if (s[j] == '1') {
-                classes[i].push_back(j);
-            }
-        }
-    }
-    memset(mn, 0x3f, sizeof(mn));
-    for (int i = 0; i < n; ++i) {
-        mn[i][0] = 0;
-        for (int j = 0; j < (int) classes[i].size(); ++j) {
-            for (int l = j; l < (int) classes[i].size(); ++l) {
-                mn[i][l - j + 1] = min(mn[i][l - j + 1], classes[i][l] - classes[i][j] + 1);
-            }
-        }
-#ifdef DEBUG
-        for (int j = 0; j <= (int) classes[i].size(); ++j) {
-            cout << mn[i][j] << " ";
-        }
-        cout << "\n";
-#endif
-    }
-    memset(dp, 0x3f, sizeof(dp));
-    for (int i = 0; i < (int) classes[0].size() + 1; ++i) {
-        dp[0][i] = mn[0][classes[0].size() - i];
-    }
+    int n;
+    cin >> n;
     for (int i = 0; i < n - 1; ++i) {
-        for (int j = 0; j <= k; ++j) {
-            int cur = dp[i][j];
-            if (cur == inf) {
-                continue;
-            }
-            for (int skip = 0; skip <= (int) classes[i + 1].size() && j + skip <= k; ++skip) {
-                dp[i + 1][j + skip] = min(dp[i + 1][j + skip], cur + mn[i + 1][(int) classes[i + 1].size() - skip]);
-            }
+        int a, b;
+        cin >> a >> b;
+        --a, --b;
+        adj[a].emplace_back(b, 0);
+        adj[b].emplace_back(a, 1);
+    }
+    dfs1(0, -1);
+    dfs2(0, -1, 0);
+#ifdef DEBUG
+    cout << "down:"
+         << "\n";
+    for (int i = 0; i < n; ++i) {
+        cout << down[i] << " ";
+    }
+    cout << "\n";
+    cout << "up:"
+         << "\n";
+    for (int i = 0; i < n; ++i) {
+        cout << up[i] << " ";
+    }
+    cout << "\n";
+#endif
+    pair<int, set<int>> sol;
+    sol.first = inf;
+    for (int i = 0; i < n; ++i) {
+        int cur = down[i] + up[i];
+        if (cur == sol.first) {
+            sol.second.insert(i + 1);
+        } else if (cur < sol.first) {
+            sol.first = cur;
+            sol.second = set<int>{i + 1};
         }
     }
-    int sol = inf;
-    for (int i = 0; i <= k; ++i) {
-        sol = min(sol, dp[n - 1][i]);
-    }
-    cout << sol << "\n";
+    cout << sol.first << "\n";
+    print(sol.second);
     return 0;
 }
