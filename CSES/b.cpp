8fd3deca7a5f62272ceaf6ae39e639c01e743745
Ian Zhang <ianzhang0510@gmail.com>
2021-03-30 09:02:10 -0400

    Solved Codeforces Round #627 (Div. 3) - E. Sleeping Schedule - https://codeforces.com/contest/1324/problem/E

--- CSES/b.cpp
+++ CSES/b.cpp
@@ -14,73 +14,44 @@ void setIO(string name) {
     freopen((name + ".out").c_str(), "w", stderr);
 #endif
 }
-const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 2e5 + 5;
-vector<int> adj[maxn];
-int a[maxn], down[maxn], up[maxn];
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 2e3 + 5;
 
-void dfs1(int c, int p) {
-    down[c] = a[c];
-    for (auto i : adj[c]) {
-        if (i != p) {
-            dfs1(i, c);
-            if (down[i] > 0) {
-                down[c] += down[i];
-            }
-        }
-    }
-}
-
-void dfs2(int c, int p) {
-    if (c != 0) {
-        up[c] = up[p] + down[p];
-        if (down[c] > 0) {
-            up[c] -= down[c];
-        }
-        up[c] = max(0, up[c]);
-    }
-    for (auto i : adj[c]) {
-        if (i != p) {
-            dfs2(i, c);
-        }
-    }
-}
+int dp[maxn][maxn];
 
 int main() {
     setIO("b");
-    int n;
-    cin >> n;
+
+    int n, h, l, r;
+    cin >> n >> h >> l >> r;
+    vector<int> a(n);
     for (int i = 0; i < n; ++i) {
         cin >> a[i];
-        if (a[i] == 0) {
-            --a[i];
+    }
+    memset(dp, -1, sizeof(dp));
+    dp[0][0] = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < h; ++j) {
+            int cur = dp[i][j];
+            if (cur == -1) {
+                continue;
+            }
+            int b = (j + a[i] - 1) % h, c = (j + a[i]) % h;
+            if (b >= l && b <= r) {
+                dp[i + 1][b] = max(dp[i + 1][b], cur + 1);
+            } else {
+                dp[i + 1][b] = max(dp[i + 1][b], cur);
+            }
+            if (c >= l && c <= r) {
+                dp[i + 1][c] = max(dp[i + 1][c], cur + 1);
+            } else {
+                dp[i + 1][c] = max(dp[i + 1][c], cur);
+            }
         }
     }
-    for (int i = 0; i < n - 1; ++i) {
-        int b, c;
-        cin >> b >> c;
-        --b, --c;
-        adj[b].push_back(c);
-        adj[c].push_back(b);
+    int sol = 0;
+    for (int i = 0; i < h; ++i) {
+        sol = max(sol, dp[n][i]);
     }
-    dfs1(0, -1);
-    dfs2(0, -1);
-#ifdef DEBUG
-    cout << "down:"
-         << "\n";
-    for (int i = 0; i < n; ++i) {
-        cout << down[i] << " ";
-    }
-    cout << "\n";
-    cout << "up:"
-         << "\n";
-    for (int i = 0; i < n; ++i) {
-        cout << up[i] << " ";
-    }
-    cout << "\n";
-#endif
-    for (int i = 0; i < n; ++i) {
-        cout << down[i] + up[i] << " ";
-    }
-    cout << "\n";
+    cout << sol << "\n";
     return 0;
 }
