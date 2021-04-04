788aa3c6b2f561df5582071fa68d5341edf1c69b
Ian Zhang <ianzhang0510@gmail.com>
2021-04-03 22:40:39 -0400

    Solved Educational Codeforces Round 26 - D. Round Subset - https://codeforces.com/contest/837/problem/D

--- CSES/b.cpp
+++ CSES/b.cpp
@@ -14,65 +14,59 @@ void setIO(string name) {
     freopen((name + ".out").c_str(), "w", stderr);
 #endif
 }
-const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 2e5 + 5;
-vector<pair<int, int>> adj[maxn];
-long long down[maxn][2], up[maxn][2];
+const int inf = 0x3f3f3f3f, mod = 998244353, maxn = 205, maxs = 30;
 
-void dfs1(int c, int p) {
-    for (auto i : adj[c]) {
-        if (i.first != p) {
-            dfs1(i.first, c);
-            if (i.second == 0) {
-                down[c][0] += 1 + down[i.first][0];
-            } else {
-                down[c][1] += 1 + down[i.first][0] + down[i.first][1];
-            }
-        }
-    }
-}
+int dp[2][maxn][maxn * maxs];
 
-void dfs2(int c, int p, int w) {
-    if (p != -1) {
-        array<long long, 2> d{down[p][0], down[p][1]};
-        if (w == 0) {
-            up[c][0] += 1 + up[p][0];
-            d[0] -= 1 + down[c][0];
-            up[c][0] += d[0];
-        } else {
-            up[c][1] += 1 + up[p][0] + up[p][1];
-            d[1] -= 1 + down[c][0] + down[c][1];
-            up[c][1] += d[0] + d[1];
-        }
+pair<int, int> fact(long long b) {
+    pair<int, int> res;
+    while (b % 2 == 0) {
+        ++res.first;
+        b /= 2;
     }
-    for (auto i : adj[c]) {
-        if (i.first != p) {
-            dfs2(i.first, c, i.second);
-        }
+    while (b % 5 == 0) {
+        ++res.second;
+        b /= 5;
     }
+    return res;
 }
 
 int main() {
     setIO("b");
 
-    int n;
-    cin >> n;
-    for (int i = 0; i < n - 1; ++i) {
-        int a, b, c;
-        cin >> a >> b >> c;
-        --a, --b;
-        adj[a].emplace_back(b, c);
-        adj[b].emplace_back(a, c);
-    }
-    dfs1(0, -1);
-    dfs2(0, -1, -1);
-#ifdef DEBUG
-    for (int j = 0; j < n; ++j) {
-        cout << down[j][0] << " " << down[j][1] << " " << up[j][0] << " " << up[j][1] << "\n";
-    }
-#endif
-    long long sol = 0;
+    int n, k;
+    cin >> n >> k;
+    vector<pair<int, int>> a(n);
+
     for (int i = 0; i < n; ++i) {
-        sol += down[i][0] + down[i][1] + up[i][0] + up[i][1];
+        long long b;
+        cin >> b;
+        a[i] = fact(b);
+#ifdef DEBUG
+        cout << b << " " << a[i].first << " " << a[i].second << "\n";
+#endif
+    }
+    memset(dp, -1, sizeof(dp));
+    dp[0][0][0] = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j <= k; ++j) {
+            for (int l = 0; l < maxn * maxs; ++l) {
+                int cur = dp[i % 2][j][l];
+                if (cur == -1) {
+                    continue;
+                }
+#ifdef DEBUG
+                cout << i << " " << j << " " << l << " " << cur << "\n";
+#endif
+                dp[(i + 1) % 2][j][l] = max(cur, dp[(i + 1) % 2][j][l]);
+                dp[(i + 1) % 2][j + 1][l + a[i].second] = max(dp[(i + 1) % 2][j + 1][l + a[i].second], cur + a[i].first);
+            }
+        }
+        memset(dp[i % 2], -1, sizeof(dp[i % 2]));
+    }
+    int sol = 0;
+    for (int i = 0; i < maxn * maxs; ++i) {
+        sol = max(sol, min(i, dp[n % 2][k][i]));
     }
     cout << sol << "\n";
     return 0;
