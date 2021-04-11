15130eaaf0a2345131e82ea5cb6bd2db1a2935c9
Ian Zhang <ianzhang0510@gmail.com>
2021-04-11 16:12:17 -0400

    Solved Topic Stream Mashup: DP Optimizations (segment tree, convex hull trick) - E. Kalila and Dimna in the Logging Industry - https://codeforces.com/gym/316406/problem/E, https://codeforces.com/problemset/problem/319/C

--- Codeforces/Topic Stream Mashup: DP Optimizations (segment tree, convex hull trick)/X E. Kalila and Dimna in the Logging Industry/e.cpp
+++ Codeforces/Topic Stream Mashup: DP Optimizations (segment tree, convex hull trick)/X E. Kalila and Dimna in the Logging Industry/e.cpp
@@ -16,15 +16,18 @@ void setIO(string name) {
 }
 const int inf = 0x3f3f3f3f, mod = 1e9 + 7;
 
-
 const long long is_query = -(1LL << 62);
 struct line {
     long long m, b;
     mutable function<const line *()> succ;
     bool operator<(const line &rhs) const {
-        if (rhs.b != is_query) return m < rhs.m;
+        if (rhs.b != is_query) {
+            return m < rhs.m;
+        }
         const line *s = succ();
-        if (!s) return 0;
+        if (!s) {
+            return 0;
+        }
         long long x = rhs.m;
         return b - s->b < (s->m - m) * x;
     }
@@ -35,21 +38,28 @@ struct dynamic_hull : public multiset<line> { // will maintain upper hull for ma
     bool bad(iterator y) {
         auto z = next(y);
         if (y == begin()) {
-            if (z == end()) return 0;
+            if (z == end()) {
+                return 0;
+            }
             return y->m == z->m && y->b <= z->b;
         }
         auto x = prev(y);
-        if (z == end()) return y->m == x->m && y->b <= x->b;
-
+        if (z == end()) {
+            return y->m == x->m && y->b <= x->b;
+        }
         /* compare two lines by slope, make sure denominator is not 0 */
         long long v1 = (x->b - y->b);
-        if (y->m == x->m) v1 = x->b > y->b ? inf : -inf;
-        else
+        if (y->m == x->m) {
+            v1 = x->b > y->b ? inf : -inf;
+        } else {
             v1 /= (y->m - x->m);
+        }
         long long v2 = (y->b - z->b);
-        if (z->m == y->m) v2 = y->b > z->b ? inf : -inf;
-        else
+        if (z->m == y->m) {
+            v2 = y->b > z->b ? inf : -inf;
+        } else {
             v2 /= (z->m - y->m);
+        }
         return v1 >= v2;
     }
     void insert_line(long long m, long long b) {
@@ -81,23 +91,25 @@ int main() {
 
     int n;
     cin >> n;
-    vector<long long> a(n), pre(n + 1), dp(n);
+    vector<long long> a(n), b(n), dp(n);
     for (int i = 0; i < n; ++i) {
         cin >> a[i];
-        pre[i + 1] = pre[i] + a[i];
     }
-    vector<int> b(n);
     for (int i = 0; i < n; ++i) {
         cin >> b[i];
     }
-    cht.insert_line(0, 0);
+    dp[0] = 0;
+    cht.insert_line(-b[0], -dp[0]);
     for (int i = 1; i < n; ++i) {
-        dp[i] = pre[i + 1] * b[i] - cht.eval(b[i]);
-        cht.insert_line(pre[i], -dp[i]);
+        dp[i] = -cht.eval(a[i]);
+        cht.insert_line(-b[i], -dp[i]);
     }
+#ifdef DEBUG
     for (int i = 0; i < n; ++i) {
         cout << dp[i] << " ";
     }
     cout << "\n";
+#endif
+    cout << dp[n - 1] << "\n";
     return 0;
 }
