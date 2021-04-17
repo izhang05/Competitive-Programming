11896e712ade91a313c7e70860405344258aaf71
Ian Zhang <ianzhang0510@gmail.com>
2021-04-17 11:54:17 -0400

    Solved Educational Codeforces Round 93 (Rated for Div. 2) - C. Good Subarrays - https://codeforces.com/contest/1398/problem/C

--- CSES/a.cpp
+++ CSES/a.cpp
@@ -2,8 +2,6 @@
 
 using namespace std;
 
-mt19937 rng((uint32_t) chrono::steady_clock::now().time_since_epoch().count());
-
 //#define DEBUG
 void setIO(string name) {
     ios_base::sync_with_stdio(false);
@@ -18,101 +16,34 @@ void setIO(string name) {
 
 const int inf = 0x3f3f3f3f, mod = 1e9 + 7;
 
-const long long is_query = -(1LL << 62);
-
-struct line {
-    long long m, b;
-    mutable function<const line *()> succ;
-
-    bool operator<(const line &rhs) const {
-        if (rhs.b != is_query) {
-            return m < rhs.m;
-        }
-        const line *s = succ();
-        if (!s) {
-            return false;
-        }
-        long long x = rhs.m;
-        return b - s->b < (s->m - m) * x;
-    }
-};
-
-struct dynamic_hull : public multiset<line> { // will maintain upper hull for maximum
-    const long long inf = LLONG_MAX;
-
-    bool bad(iterator y) {
-        auto z = next(y);
-        if (y == begin()) {
-            if (z == end()) {
-                return false;
-            }
-            return y->m == z->m && y->b <= z->b;
-        }
-        auto x = prev(y);
-        if (z == end()) {
-            return y->m == x->m && y->b <= x->b;
-        }
-        /* compare two lines by slope, make sure denominator is not 0 */
-        long long v1 = (x->b - y->b);
-        if (y->m == x->m) {
-            v1 = x->b > y->b ? inf : -inf;
-        } else {
-            v1 /= (y->m - x->m);
-        }
-        long long v2 = (y->b - z->b);
-        if (z->m == y->m) {
-            v2 = y->b > z->b ? inf : -inf;
-        } else {
-            v2 /= (z->m - y->m);
-        }
-        return v1 >= v2;
-    }
-
-    void insert_line(long long m, long long b) {
-        auto y = insert({m, b});
-        y->succ = [=] {
-            return next(y) == end() ? 0 : &*next(y);
-        };
-        if (bad(y)) {
-            erase(y);
-            return;
-        }
-        while (next(y) != end() && bad(next(y))) {
-            erase(next(y));
-        }
-        while (y != begin() && bad(prev(y))) {
-            erase(prev(y));
-        }
-    }
-
-    long long eval(long long x) {
-        auto l = *lower_bound((line) {x, is_query});
-        return l.m * x + l.b;
-    }
-};
 
 int main() {
     setIO("1");
 
-    int n;
-    cin >> n;
-    int a, b, c;
-    cin >> a >> b >> c;
-    vector<long long> d(n), dp(n), pre(n + 1);
-    for (int i = 0; i < n; ++i) {
-        cin >> d[i];
-        pre[i + 1] = pre[i] + d[i];
-    }
-    dynamic_hull cht;
-    cht.insert_line(0, 0);
-    for (int i = 0; i < n; ++i) {
-        long long x = pre[i + 1];
-        dp[i] = b * x + c + a * x * x + cht.eval(x);
-        cht.insert_line(-2 * x * a, dp[i] - b * x + a * x * x);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        vector<int> a(n);
+        int pre = 0;
+        map<int, long long> vals;
+        for (int i = 0; i < n; ++i) {
+            a[i] = s[i] - '0';
+            pre += a[i];
+            ++vals[pre - (i + 1)];
+        }
+        ++vals[0];
+        long long sol = 0;
+        for (auto i : vals) {
 #ifdef DEBUG
-        cout << dp[i] << "\n";
+            cout << i.first << " " << i.second << "\n";
 #endif
+            sol += (i.second * (i.second - 1)) / 2;
+        }
+        cout << sol << "\n";
     }
-    cout << dp[n - 1] << "\n";
     return 0;
 }
