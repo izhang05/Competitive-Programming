3f5b8b39e6a341d6e2d652b2bbbf76478d68bd9a
Ian Zhang <ianzhang0510@gmail.com>
2021-04-30 10:49:59 -0400

    Solved VK Cup 2015 - Round 2 (unofficial online mirror, Div. 1 only) - E. Correcting Mistakes - https://codeforces.com/contest/533/problem/E

--- CSES/a.cpp
+++ CSES/a.cpp
@@ -15,48 +15,35 @@ void setIO(string name) {
 }
 
 const int mod = 1e9 + 7, maxn = 1e5 + 5;
-const long long inf = 1e18;
 
-long long dp[maxn][2];
+int n;
+
+int solve(string s, string t) {
+    int i = 0;
+    while (i < n && s[i] == t[i]) {
+        ++i;
+    }
+    int j = n - 1;
+    while (j >= 0 && s[j] == t[j]) {
+        --j;
+    }
+    string a;
+    for (int k = i + 1; k < j + 1; ++k) {
+        a += s[k];
+    }
+    string b;
+    for (int k = i; k < j; ++k) {
+        b += t[k];
+    }
+    return a == b;
+}
 
 int main() {
     setIO("1");
-
-    int n;
     cin >> n;
-    vector<int> c(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> c[i];
-    }
-    vector<vector<string>> s(n, vector<string>(2));
-    for (int i = 0; i < n; ++i) {
-        cin >> s[i][0];
-        s[i][1] = s[i][0];
-        reverse(s[i][1].begin(), s[i][1].end());
-    }
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < 2; ++j) {
-            dp[i][j] = inf;
-        }
-    }
-    dp[0][0] = 0;
-    dp[0][1] = c[0];
-    for (int i = 1; i < n; ++i) {
-        if (s[i][0] >= s[i - 1][0]) {
-            dp[i][0] = min(dp[i][0], dp[i - 1][0]);
-        }
-        if (s[i][0] >= s[i - 1][1]) {
-            dp[i][0] = min(dp[i][0], dp[i - 1][1]);
-        }
-        if (s[i][1] >= s[i - 1][0]) {
-            dp[i][1] = min(dp[i][1], dp[i - 1][0] + c[i]);
-        }
-        if (s[i][1] >= s[i - 1][1]) {
-            dp[i][1] = min(dp[i][1], dp[i - 1][1] + c[i]);
-        }
-    }
-    long long sol = min(dp[n - 1][0], dp[n - 1][1]);
-    cout << (sol == inf ? -1 : sol) << "\n";
+    string s, t;
+    cin >> s >> t;
+    cout << solve(s, t) + solve(t, s) << "\n";
     return 0;
 }
 
