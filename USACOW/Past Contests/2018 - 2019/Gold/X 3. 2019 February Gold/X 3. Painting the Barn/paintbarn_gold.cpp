92457eeb73ac74a2dcb70bb62d998088d8ce8f02
Ian Zhang <ianzhang0510@gmail.com>
2021-04-29 23:42:34 -0400

    Solved USACO 2019 February Gold - 3. Painting the Barn - http://www.usaco.org/index.php?page=viewproblem2&cpid=923

--- USACOW/Past Contests/2018 - 2019/Gold/X 3. 2019 February Gold/X 3. Painting the Barn/paintbarn_gold.cpp
+++ USACOW/Past Contests/2018 - 2019/Gold/X 3. 2019 February Gold/X 3. Painting the Barn/paintbarn_gold.cpp
@@ -11,39 +11,148 @@ using namespace std;
 void setIO() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+    cin.exceptions(istream::failbit);
     freopen("paintbarn.in", "r", stdin);
     freopen("paintbarn.out", "w", stdout);
+    freopen("paintbarn.out", "w", stderr);
 }
-
+//#define DEBUG
 const int maxn = 201;
-int pre[maxn][maxn], grid[maxn][maxn];
+int diff[maxn][maxn], grid[maxn][maxn], pre[maxn][maxn];
+
+int lef[maxn], rig[maxn], up[maxn], down[maxn];
+
+int get(int row, int l, int r) {
+    if (l == 0) {
+        return pre[row][r];
+    }
+    return pre[row][r] - pre[row][l - 1];
+}
 
 int main() {
     setIO();
     int n, k;
     cin >> n >> k;
-    int x1, y1, x2, y2;
     for (int i = 0; i < n; ++i) {
-        cin >> x1 >> y1 >> x2 >> y2;
-        ++x1, ++y1, ++x2, ++y2;
-        ++pre[x1][y1], --pre[x2][y1], --pre[x1][y2], ++pre[x2][y2];
+        pair<int, int> x, y;
+        cin >> x.first >> x.second >> y.first >> y.second;
+        for (int j = x.first; j < y.first; ++j) {
+            ++diff[j][x.second];
+            --diff[j][y.second];
+        }
     }
-
-    int sol = 0;
-    for (int i = 1; i < maxn; ++i) {
+#ifdef DEBUG
+    cout << "diff: "
+         << "\n";
+    for (int i = 1; i < 10; ++i) {
+        for (int j = 1; j < 10; ++j) {
+            cout << diff[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+#endif
+    int orig = 0;
+    for (int i = 0; i < maxn; ++i) {
+        int cur = diff[i][0];
         for (int j = 1; j < maxn; ++j) {
-            pre[i][j] += pre[i - 1][j] + pre[i][j - 1] - pre[i - 1][j - 1];
-            if (j < 10) {
-                cout << i - 1 << " " << j - 1 << " " << pre[i][j] << "\n";
-            }
-            if (pre[i][j] == k) {
-                grid[i][j] = -1;
-            } else if (pre[i][j] == k - 1) {
+            if (cur == k - 1) {
                 grid[i][j] = 1;
-            } else {
-                grid[i][j] = 0;
+            } else if (cur == k) {
+                grid[i][j] = -1;
+                ++orig;
+            }
+            cur += diff[i][j];
+        }
+    }
+#ifdef DEBUG
+    cout << orig << "\n";
+    cout << "grid: "
+         << "\n";
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            cout << grid[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+#endif
+    for (int i = 0; i < maxn; ++i) {
+        for (int j = 1; j < maxn; ++j) {
+            pre[i][j] = pre[i][j - 1] + grid[i][j];
+        }
+    }
+    for (int l = 1; l < maxn; ++l) {
+        for (int r = l; r < maxn; ++r) {
+            int cur = 0;
+            for (int row = 0; row < 200; ++row) {
+                if (cur < 0) {
+                    cur = 0;
+                }
+                cur += get(row, l, r);
+                up[row] = max(up[row], cur);
+                lef[r] = max(lef[r], cur);
+                rig[l] = max(rig[l], cur);
+            }
+            cur = 0;
+            for (int row = 200 - 1; row >= 0; --row) {
+                if (cur < 0) {
+                    cur = 0;
+                }
+                cur += get(row, l, r);
+                down[row] = max(down[row], cur);
+                lef[r] = max(lef[r], cur);
+                rig[l] = max(rig[l], cur);
             }
         }
     }
+    int sol = 0;
+    for (int i = 1; i < maxn; ++i) {
+        lef[i] = max(lef[i], lef[i - 1]);
+        sol = max(sol, lef[i]);
+    }
+    for (int i = 1; i < maxn; ++i) {
+        up[i] = max(up[i], up[i - 1]);
+        sol = max(sol, up[i]);
+    }
+    for (int i = maxn - 2; i >= 0; --i) {
+        rig[i] = max(rig[i], rig[i + 1]);
+        sol = max(sol, rig[i]);
+    }
+    for (int i = maxn - 2; i >= 0; --i) {
+        down[i] = max(down[i], down[i + 1]);
+        sol = max(sol, down[i]);
+    }
+    for (int i = 1; i < maxn; ++i) {
+        sol = max(sol, up[i - 1] + down[i]);
+        sol = max(sol, lef[i - 1] + rig[i]);
+    }
+#ifdef DEBUG
+    cout << "lef:"
+         << "\n";
+    for (int i = 0; i < 8; ++i) {
+        cout << lef[i] << " ";
+    }
+    cout << "\n";
+    cout << "rig:"
+         << "\n";
+    for (int i = 0; i < 8; ++i) {
+        cout << rig[i] << " ";
+    }
+    cout << "\n";
+    cout << "up:"
+         << "\n";
+    for (int i = 0; i < 8; ++i) {
+        cout << up[i] << " ";
+    }
+    cout << "\n";
+    cout << "down:"
+         << "\n";
+    for (int i = 0; i < 8; ++i) {
+        cout << down[i] << " ";
+    }
+    cout << "\n";
+#endif
+    cout << orig + sol << "\n";
     return 0;
 }
