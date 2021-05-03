1ada5fc15936e5324334058732d4fdac1d9960bd
Ian Zhang <ianzhang0510@gmail.com>
2021-05-03 17:37:06 -0400

    Solved CSES - Missing Coin Sum - https://cses.fi/problemset/task/2183/

---
+++ CSES/X 2. Sorting and Searching/Y 10. Missing Coin Sum.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//#define DEBUG
+void setIO(const string &name) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin.exceptions(istream::failbit);
+#ifdef LOCAL
+    freopen((name + ".in").c_str(), "r", stdin);
+    freopen((name + ".out").c_str(), "w", stdout);
+    freopen((name + ".out").c_str(), "w", stderr);
+#endif
+}
+
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7;
+
+
+int main() {
+    setIO("10");
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    long long sol = 0;
+    for (int i = 0; i < n; ++i) {
+        if (a[i] > sol + 1) {
+            break;
+        }
+        sol += a[i];
+    }
+    cout << sol + 1 << "\n";
+    return 0;
+}
