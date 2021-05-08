4b9b6d9a4e0c8c7536bbc57ce5724043b38165da
Ian Zhang <ianzhang0510@gmail.com>
2021-05-08 14:51:07 -0400

    Solved Codeforces Round #529 (Div. 3) - E. Almost Regular Bracket Sequence - https://codeforces.com/contest/1095/problem/E

--- CSES/a.cpp
+++ CSES/a.cpp
@@ -9,90 +9,12 @@ void setIO(const string &name) {
     cin.exceptions(istream::failbit);
 #ifdef LOCAL
     freopen((name + ".in").c_str(), "r", stdin);
-    freopen((name + ".out").c_str(), "w", stdout);
-    freopen((name + ".out").c_str(), "w", stderr);
+//    freopen((name + ".out").c_str(), "w", stdout);
+//    freopen((name + ".out").c_str(), "w", stderr);
 #endif
 }
 
-const int mod = 1e9 + 7;
-const long long inf = 1e18;
-
-struct item {
-    long long gcd, mn, cnt;
-};
-
-
-struct segtree {
-    int size{};
-    vector<item> t;
-    item neutral = {-1, inf, 0};
-
-    void init(int n) {
-        size = 1;
-        while (size < n) {
-            size *= 2;
-        }
-        t.resize(2 * size);
-    }
-
-    static item merge(item a, item b) {
-        item res{};
-
-        if (a.gcd == -1 || b.gcd == -1) {
-            res.gcd = max(a.gcd, b.gcd);
-        } else {
-            res.gcd = __gcd(a.gcd, b.gcd);
-        }
-        res.mn = min(a.mn, b.mn);
-        if (a.mn == b.mn) {
-            res.cnt = a.cnt + b.cnt;
-        } else if (a.mn < b.mn) {
-            res.cnt = a.cnt;
-        } else {
-            res.cnt = b.cnt;
-        }
-        return res;
-    };
-
-    static item single(int v) {
-        return {v, v, 1};
-    }
-
-    void upd(int p, int v, int x, int lx, int rx) {
-        if (rx - lx == 1) {
-            t[x] = single(v);
-            return;
-        }
-        int m = (lx + rx) / 2;
-        if (p < m) {
-            upd(p, v, 2 * x + 1, lx, m);
-        } else {
-            upd(p, v, 2 * x + 2, m, rx);
-        }
-        t[x] = merge(t[2 * x + 1], t[2 * x + 2]);
-    }
-
-    void upd(int p, int v) {
-        upd(p, v, 0, 0, size);
-    }
-
-    item query(int l, int r, int x, int lx, int rx) {
-        if (lx >= r || rx <= l) {
-            return neutral;
-        }
-        if (lx >= l && rx <= r) {
-            return t[x];
-        }
-        int m = (lx + rx) / 2;
-
-        return merge(query(l, r, 2 * x + 1, lx, m), query(l, r, 2 * x + 2, m, rx));
-    }
-
-    item query(int l, int r) {
-        return query(l, r, 0, 0, size);
-    }
-
-};
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7;
 
 
 int main() {
@@ -100,25 +22,64 @@ int main() {
 
     int n;
     cin >> n;
-    segtree seg;
-    seg.init(n);
-    for (int i = 0; i < n; ++i) {
-        int a;
-        cin >> a;
-        seg.upd(i, a);
+    string s;
+    cin >> s;
+    if (n % 2 != 0) {
+        cout << 0 << "\n";
+        return 0;
     }
-    int q;
-    cin >> q;
-    while (q--) {
-        int l, r;
-        cin >> l >> r;
-        --l, --r;
-        item cur = seg.query(l, r + 1);
-        long long sub = 0;
-        if (cur.mn == cur.gcd) {
-            sub = cur.cnt;
+    int l = 0, r = 0;
+    for (int i = 0; i < n; ++i) {
+        if (s[i] == '(') {
+            ++l;
+        } else {
+            ++r;
         }
-        cout << r - l + 1 - sub << "\n";
+    }
+    if (abs(l - r) != 2) {
+        cout << 0 << "\n";
+        return 0;
+    }
+    if (l > r) {
+        int cur = 0, sol = 0;
+        for (int i = 0; i < n; ++i) {
+            if (s[i] == '(') {
+                if (cur > 0) {
+                    ++sol;
+                }
+                ++cur;
+            } else {
+                --cur;
+                if (cur < 0) {
+                    sol = 0;
+                    break;
+                }
+                if (cur == 1) {
+                    sol = 0;
+                }
+            }
+        }
+        cout << sol << "\n";
+    } else {
+        int cur = 0, sol = 0;
+        for (int i = n - 1; i >= 0; --i) {
+            if (s[i] == '(') {
+                ++cur;
+                if (cur > 0) {
+                    sol = 0;
+                    break;
+                }
+                if (cur == -1) {
+                    sol = 0;
+                }
+            } else {
+                if (cur < 0) {
+                    ++sol;
+                }
+                --cur;
+            }
+        }
+        cout << sol << "\n";
     }
     return 0;
 }
