081e7127bc34af92375a6f92713f825840bb3d33
Ian Zhang <ianzhang0510@gmail.com>
2021-05-10 10:58:35 -0400

    Solved Educational Codeforces Round 31 - F. Anti-Palindromize - https://codeforces.com/contest/884/problem/F

--- CSES/a.cpp
+++ CSES/a.cpp
@@ -14,89 +14,50 @@ void setIO(const string &name) {
 #endif
 }
 
-const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 20000;
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7;
 
-int parent[2 * maxn], grid[2][maxn], ind[2 * maxn];
-
-int get(int x) {
-    return parent[x] == x ? x : parent[x] = get(parent[x]);
-}
-
-bool merge(int x, int y) {
-    int xroot = get(x), yroot = get(y);
-    if (xroot != yroot) {
-        parent[xroot] = yroot;
-        return true;
-    }
-    return false;
-}
-
-bool same(int x, int y) {
-    return get(x) == get(y);
-}
 
 int main() {
     setIO("1");
 
-    int n, m;
-    cin >> n >> m;
-    for (int i = 0; i < 2 * m; ++i) {
-        parent[i] = i;
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
     }
     int sol = 0;
-    for (int i = 0; i < n; ++i) {
+    map<char, int> occ;
+    for (int i = 0; i < n / 2; ++i) {
+        if (s[i] != s[n - i - 1]) {
+            sol += a[i] + a[n - i - 1];
+        } else {
+            sol += max(a[i], a[n - i - 1]);
+            ++occ[s[i]];
+        }
+    }
+    int total = 0;
+    pair<int, char> mx;
+    for (auto &i : occ) {
+        total += i.second;
+        mx = max(mx, {i.second, i.first});
+    }
+    mx.first *= 2;
+    mx.first -= total;
+    priority_queue<int, vector<int>, greater<>> unused;
+    for (int i = 0; i < n / 2; ++i) {
+        if (s[i] != s[n - i - 1] && s[i] != mx.second && s[n - i - 1] != mx.second) {
 #ifdef DEBUG
-        for (int j = 0; j < m; ++j) {
-          cout << parent[j] << " ";
-        }
-        cout << "\n";
+            cout << s[i] << " " << min(a[i], a[n - i - 1]) << "\n";
 #endif
-        for (int j = 0; j < m / 4; ++j) {
-            char c;
-            cin >> c;
-            int a = (c >= 'A') ? (c - 'A' + 10) : (c - '0');
-            for (int k = 0; k < 4; ++k) {
-                bool cur = a & (1 << k);
-                int col = 4 * j + 4 - k - 1;
-                grid[i % 2][col] = cur;
-            }
-            for (int k = j * 4; k < j * 4 + 4; ++k) {
-                bool cur = grid[i % 2][k];
-                if (cur) {
-                    ++sol;
-                    if (grid[(i + 1) % 2][k]) {
-                        if (merge(k, k + m)) {
-                            --sol;
-                        }
-                    }
-                    if (k > 0 && grid[i % 2][k - 1]) {
-                        if (merge(k + m - 1, k + m)) {
-                            --sol;
-                        }
-                    }
-                }
-#ifdef DEBUG
-                cout << i << " " << k << " " << sol << "\n";
-#endif
-            }
+            unused.push(min(a[i], a[n - i - 1]));
         }
-        for (int j = 0; j < 2 * m; ++j) {
-            get(j);
-            ind[j] = -1;
-        }
-#ifdef DEBUG
-        for (int j = m; j < 2*m; ++j) {
-          cout << parent[j] << " ";
-        }
-        cout << "\n";
-#endif
-        for (int j = m; j < 2 * m; ++j) {
-            if (ind[parent[j]] == -1) {
-                ind[parent[j]] = j - m;
-            }
-            parent[j - m] = ind[parent[j]];
-            parent[j] = j;
-        }
+    }
+    for (int i = 0; i < mx.first; ++i) {
+        sol -= unused.top();
+        unused.pop();
     }
     cout << sol << "\n";
     return 0;
