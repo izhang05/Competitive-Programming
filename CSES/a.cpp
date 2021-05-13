4837ebad129416be519a6824d55e5dbca437c86c
Ian Zhang <ianzhang0510@gmail.com>
2021-05-13 17:11:00 -0400

    Solved Educational Codeforces Round 31 - C. Bertown Subway - https://codeforces.com/contest/884/problem/C

--- CSES/a.cpp
+++ CSES/a.cpp
@@ -14,23 +14,47 @@ void setIO(const string &name) {
 #endif
 }
 
-const int inf = 0x3f3f3f3f, mod = 1e9 + 7;
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 1e5 + 5;
 
+int ne[maxn];
+bool visited[maxn];
+priority_queue<long long> s;
+
+void dfs(int c, int d) {
+    if (visited[c]) {
+        s.push(d);
+        return;
+    }
+    visited[c] = true;
+    dfs(ne[c], d + 1);
+}
 
 int main() {
     setIO("1");
 
-    int n, t;
-    cin >> n >> t;
-    int sum = 0;
+    int n;
+    cin >> n;
     for (int i = 0; i < n; ++i) {
-        int a;
-        cin >> a;
-        sum += 86400 - a;
-        if (sum >= t) {
-            cout << i + 1 << "\n";
-            return 0;
+        cin >> ne[i];
+        --ne[i];
+    }
+    for (int i = 0; i < n; ++i) {
+        if (!visited[i]) {
+            dfs(i, 0);
         }
     }
+    long long cur = 0;
+    for (int i = 0; i < 2; ++i) {
+        if (!s.empty()) {
+            cur += s.top();
+            s.pop();
+        }
+    }
+    long long sol = cur * cur;
+    while (!s.empty()) {
+        sol += s.top() * s.top();
+        s.pop();
+    }
+    cout << sol << "\n";
     return 0;
 }
