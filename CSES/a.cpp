8d4e6d20f58abbee82d20a27fbfe0e63e96c0e66
Ian Zhang <ianzhang0510@gmail.com>
2021-05-13 14:16:33 -0400

    Solved Educational Codeforces Round 31 - B. Japanese Crosswords Strike Back - https://codeforces.com/contest/884/problem/B

--- CSES/a.cpp
+++ CSES/a.cpp
@@ -14,61 +14,24 @@ void setIO(const string &name) {
 #endif
 }
 
-const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 105;
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7;
 
-int dp[maxn][maxn][maxn][2]; // dp[pos][even][odd][last]
 
 int main() {
     setIO("1");
 
-    int n;
-    cin >> n;
-    if (n == 1) {
-        cout << 0 << "\n";
-        return 0;
-    }
-    vector<int> a(n);
-    int even = n / 2, odd = (n + 1) / 2;
+    int n, x;
+    cin >> n >> x;
+    long long sum = 0;
     for (int i = 0; i < n; ++i) {
-        cin >> a[i];
-        if (a[i] != 0) {
-            if (a[i] % 2 == 0) {
-                --even;
-            } else {
-                --odd;
-            }
-        }
+        int a;
+        cin >> a;
+        sum += a;
     }
-#ifdef DEBUG
-    cout << even << " " << odd << "\n";
-#endif
-    memset(dp, 0x3f, sizeof(dp));
-    dp[0][even][odd][0] = dp[0][even][odd][1] = 0;
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            for (int k = 0; k < n; ++k) {
-                for (int l = 0; l < 2; ++l) {
-                    int cur = dp[i][j][k][l];
-                    if (cur == inf) {
-                        continue;
-                    }
-#ifdef DEBUG
-                    cout << i << " " << j << " " << k << " " << l << " " << cur << "\n";
-#endif
-                    if (a[i] != 0) {
-                        dp[i + 1][j][k][a[i] % 2] = min(dp[i + 1][j][k][a[i] % 2], cur + (l != a[i] % 2));
-                    } else {
-                        if (j > 0) {
-                            dp[i + 1][j - 1][k][0] = min(dp[i + 1][j - 1][k][0], cur + (l != 0));
-                        }
-                        if (k > 0) {
-                            dp[i + 1][j][k - 1][1] = min(dp[i + 1][j][k - 1][1], cur + (l != 1));
-                        }
-                    }
-                }
-            }
-        }
+    if (sum + n - 1 == x) {
+        cout << "YES" << "\n";
+    } else {
+        cout << "NO" << "\n";
     }
-    cout << min(dp[n][0][0][0], dp[n][0][0][1]) << "\n";
     return 0;
 }
