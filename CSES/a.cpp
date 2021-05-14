4b489582b7ba1365cceafb9205f0237df132bdab
Ian Zhang <ianzhang0510@gmail.com>
2021-05-14 11:52:23 -0400

    Solved Codeforces Round #702 (Div. 3) - E. Accidental Victory - https://codeforces.com/contest/1490/problem/E

--- CSES/a.cpp
+++ CSES/a.cpp
@@ -14,109 +14,79 @@ void setIO(const string &name) {
 #endif
 }
 
-const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 105;
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7;
 
-struct item {
-    int mx, ind;
-};
-
-struct segtree {
-    int size{};
-    vector<item> t;
-    item neutral = {0, 0};
-
-    void init(int n) {
-        size = 1;
-        while (size < n) {
-            size *= 2;
-        }
-        t.clear();
-        t.resize(2 * size);
+template<class T>
+void print(T a, string sep = " ", string end = "\n") {
+    for (auto i : a) {
+        cout << i << sep;
     }
-
-    static item merge(item a, item b) {
-        item res{};
-        res.mx = max(a.mx, b.mx);
-        if (a.mx > b.mx) {
-            res.ind = a.ind;
-        } else {
-            res.ind = b.ind;
-        }
-        return res;
-    }
-
-    static item single(int v, int p) {
-        return {v, p};
-    }
-
-    void upd(int p, int v, int x, int lx, int rx) {
-        if (rx - lx == 1) {
-            t[x] = single(v, p);
-            return;
-        }
-        int m = (lx + rx) / 2;
-        if (p < m) {
-            upd(p, v, 2 * x + 1, lx, m);
-        } else {
-            upd(p, v, 2 * x + 2, m, rx);
-        }
-        t[x] = merge(t[2 * x + 1], t[2 * x + 2]);
-    }
-
-    void upd(int p, int v) {
-        upd(p, v, 0, 0, size);
-    }
-
-    item query(int l, int r, int x, int lx, int rx) {
-        if (lx >= r || rx <= l) {
-            return neutral;
-        }
-        if (lx >= l && rx <= r) {
-            return t[x];
-        }
-        int m = (lx + rx) / 2;
-
-        return merge(query(l, r, 2 * x + 1, lx, m), query(l, r, 2 * x + 2, m, rx));
-    }
-
-    item query(int l, int r) {
-        return query(l, r, 0, 0, size);
-    }
-};
-
-int sol[maxn];
-segtree seg;
-
-void solve(int l, int r, int d) { // [l,r)
-    if (r - l < 1) {
-        return;
-    }
-    item cur = seg.query(l, r);
-    sol[cur.ind] = d;
-    solve(l, cur.ind, d + 1);
-    solve(cur.ind + 1, r, d + 1);
+    cout << end;
 }
 
-int main() {
+int n;
+const int maxn = 2e5 + 5;
+vector<pair<long long, int>> a;
+vector<long long> pre;
+
+bool pos(int x) {
+#ifdef DEBUG
+    cout << x << ":\n";
+#endif
+    long long cur = pre[x];
+    while (x + 1 < n) {
+#ifdef DEBUG
+        cout << x << " " << cur << "\n";
+#endif
+        auto it = lower_bound(a.begin(), a.end(), make_pair(cur, inf));
+        --it;
+        int ind = (int) (it - a.begin());
+        if (ind == x) {
+            return false;
+        }
+        assert(ind > x);
+        x = ind;
+        cur = pre[x];
+    }
+    return true;
+}
+
+signed main() {
     setIO("1");
 
     int t;
     cin >> t;
     while (t--) {
-        int n;
         cin >> n;
-        seg.init(n);
-        vector<int> a(n);
+        a.resize(n);
+        pre.resize(n);
         for (int i = 0; i < n; ++i) {
-            cin >> a[i];
-            seg.upd(i, a[i]);
+            cin >> a[i].first;
+            a[i].second = i + 1;
         }
-        memset(sol, 0, sizeof(sol));
-        solve(0, n, 0);
+        sort(a.begin(), a.end());
+        pre[0] = 0;
         for (int i = 0; i < n; ++i) {
-            cout << sol[i] << " ";
+            pre[i] = pre[max(0, i - 1)] + a[i].first;
         }
-        cout << "\n";
+        vector<int> sol;
+        int lo = 0, hi = n - 1, mid, res = -1;
+        while (lo <= hi) {
+            mid = (lo + hi) / 2;
+            if (pos(mid)) {
+                hi = mid - 1;
+                res = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        assert(res != -1);
+        for (int i = res; i < n; ++i) {
+            sol.push_back(a[i].second);
+        }
+        sort(sol.begin(), sol.end());
+        cout << sol.size() << "\n";
+        print(sol);
     }
     return 0;
 }
