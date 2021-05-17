145d5a1750d91cf4a082ad4be8c04944ff2dcb9f
Ian Zhang <ianzhang0510@gmail.com>
2021-05-17 11:37:48 -0400

    Solved Codeforces Beta Round #1 - A. Theatre Square - https://codeforces.com/contest/1/problem/A

--- CSES/a.cpp
+++ CSES/a.cpp
@@ -14,60 +14,14 @@ void setIO(const string &name) {
 #endif
 }
 
-const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 1e5 + 5;
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7;
 
-vector<int> adj[maxn];
-pair<int, int> mx, diam;
-
-void dfs1(int c, int p, int d) {
-    mx = max(mx, {d, c});
-    for (auto &i : adj[c]) {
-        if (i != p) {
-            dfs1(i, c, d + 1);
-        }
-    }
-}
-
-bool sol = false;
-
-void dfs2(int c, int p) {
-    if (c == diam.second) {
-        sol = true;
-        return;
-    }
-    for (auto &i : adj[c]) {
-        if (i != p) {
-            if (adj[i].size() == 1 && i == diam.second) {
-                dfs2(i, c);
-            } else if (adj[i].size() >= 3) {
-                dfs2(i, c);
-            }
-        }
-    }
-
-}
 
 int main() {
     setIO("1");
 
-    int n;
-    cin >> n;
-    for (int i = 0; i < n - 1; ++i) {
-        int a, b;
-        cin >> a >> b;
-        --a, --b;
-        adj[a].push_back(b);
-        adj[b].push_back(a);
-    }
-    dfs1(0, -1, 0);
-    diam.first = mx.second;
-    mx = {0, 0};
-    dfs1(diam.first, -1, 0);
-    diam.second = mx.second;
-#ifdef DEBUG
-    cout << diam.first << " " << diam.second << "\n";
-#endif
-    dfs2(diam.first, -1);
-    cout << (sol ? "YES" : "NO") << "\n";
+    long long n, m, a;
+    cin >> n >> m >> a;
+    cout << ((n + a - 1) / a) * ((m + a - 1) / a) << "\n";
     return 0;
 }
