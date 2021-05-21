57ae7da0b35d499b5f3c1d614280793c8f7a743f
Ian Zhang <ianzhang0510@gmail.com>
2021-05-21 09:15:23 -0400

    Solved Codeforces Round #568 (Div. 2) - G1. Playlist for Polycarp (easy version) - https://codeforces.com/contest/1185/problem/G1

--- CSES/a.cpp
+++ CSES/a.cpp
@@ -13,26 +13,59 @@ void setIO(const string &name) {
     freopen((name + ".out").c_str(), "w", stderr);
 #endif
 }
-const int inf = 0x3f3f3f3f, mod = 1e9 + 7;
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 16, maxg = 3;
 
-long long md(long long a, long long p) {
-    return (a % p + p) % p;
-}
+long long dp[1 << maxn][maxg], ti[1 << maxn];
 
 int main() {
     setIO("1");
 
-    long long n, p, k;
-    cin >> n >> p >> k;
-    map<long long, long long> cnt;
+    int n, t;
+    cin >> n >> t;
+    vector<pair<int, int>> a(n);
     for (int i = 0; i < n; ++i) {
-        long long a;
-        cin >> a;
-        ++cnt[md(((a * a) % p) * ((a * a) % p) - a * k, p)];
+        cin >> a[i].first >> a[i].second;
+        --a[i].second;
+        dp[1 << i][a[i].second] = 1;
     }
+    for (int mask = 1; mask < (1 << n); ++mask) {
+        for (int i = 0; i < n; ++i) {
+            if (mask & (1 << i)) {
+                ti[mask] = ti[mask ^ (1 << i)] + a[i].first;
+#ifdef DEBUG
+                cout << mask << " " << ti[mask] << "\n";
+#endif
+                break;
+            }
+        }
+    }
+    for (int mask = 1; mask < (1 << n); ++mask) {
+        for (int i = 0; i < maxg; ++i) {
+            long long cur = dp[mask][i];
+            if (!cur) {
+                continue;
+            }
+#ifdef DEBUG
+            cout << mask << " " << i << " " << cur << "\n";
+#endif
+            for (int k = 0; k < n; ++k) {
+                if (!(mask & (1 << k)) && a[k].second != i) {
+                    int ne = mask | (1 << k);
+                    dp[ne][a[k].second] += cur;
+                    dp[ne][a[k].second] %= mod;
+                }
+            }
+        }
+    }
+
     long long sol = 0;
-    for (auto &i : cnt) {
-        sol += ((i.second) * (i.second - 1)) / 2;
+    for (int i = 0; i < (1 << n); ++i) {
+        for (int j = 0; j < maxg; ++j) {
+            if (ti[i] == t) {
+                sol += dp[i][j];
+                sol %= mod;
+            }
+        }
     }
     cout << sol << "\n";
     return 0;
