d2721df81c270e2cc403e601387204f9385c97d0
Ian Zhang <ianzhang0510@gmail.com>
2021-05-23 16:54:19 -0400

    Solved Codeforces Round #565 (Div. 3) - E. Cover it! - https://codeforces.com/contest/1176/problem/E

--- CSES/c.cpp
+++ CSES/c.cpp
@@ -13,97 +13,58 @@ void setIO(const string &name) {
     freopen((name + ".out").c_str(), "w", stderr);
 #endif
 }
-const int mod = 1e9 + 7, maxn = 2e3 + 5;
-const long long inf = 1e18;
-int grid[maxn][maxn];
-
-int parent[maxn];
-vector<pair<int, int>> adj[maxn];
-long long dist[maxn][maxn];
-
-int get(int x) {
-    return parent[x] == x ? x : parent[x] = get(parent[x]);
-}
-
-bool merge(int x, int y) {
-    int xroot = get(x), yroot = get(y);
-    if (xroot != yroot) {
-        parent[xroot] = yroot;
-        return true;
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 2e5 + 5;
+template<class T>
+void print(T a, string sep = " ", string end = "\n") {
+    for (auto i : a) {
+        cout << i << sep;
     }
-    return false;
+    cout << end;
 }
 
-bool same(int x, int y) {
-    return get(x) == get(y);
-}
-
-int n;
+vector<int> adj[maxn];
+int dist[maxn];
 
 int main() {
     setIO("c");
 
-    cin >> n;
-    for (int i = 0; i < n; ++i) {
-        parent[i] = i;
-        for (int j = 0; j < n; ++j) {
-            dist[i][j] = inf;
-            cin >> grid[i][j];
-            if (i == j && grid[i][j]) { // main diagonal should be 0
-                cout << "NO"
-                     << "\n";
-                return 0;
-            }
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        for (int i = 0; i < n; ++i) {
+            adj[i].clear();
+            dist[i] = inf;
         }
-    }
-    for (int i = 0; i < n; ++i) {
-        for (int j = i + 1; j < n; ++j) {
-            if ((grid[i][j] != grid[j][i]) || grid[i][j] == 0) {
-                cout << "NO"
-                     << "\n";
-                return 0;
-            }
+        for (int i = 0; i < m; ++i) {
+            int a, b;
+            cin >> a >> b;
+            --a, --b;
+            adj[a].push_back(b);
+            adj[b].push_back(a);
         }
-    }
-    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> pq;
-    for (int i = 0; i < n; ++i) {
-        for (int j = i + 1; j < n; ++j) {
-            pq.push({grid[i][j], {i, j}});
-        }
-    }
-    while (!pq.empty()) {
-        pair<int, pair<int, int>> cur = pq.top();
-        pq.pop();
-        if (merge(cur.second.first, cur.second.second)) {
-            adj[cur.second.first].emplace_back(cur.second.second, cur.first);
-            adj[cur.second.second].emplace_back(cur.second.first, cur.first);
-        }
-    }
-    for (int i = 0; i < n; ++i) {
         queue<int> q;
-        q.push(i);
-        dist[i][i] = 0;
+        q.push(0);
+        dist[0] = 0;
+        vector<vector<int>> d(2);
+        d[0].push_back(1);
         while (!q.empty()) {
             int cur = q.front();
             q.pop();
-            for (auto &j : adj[cur]) {
-                if (dist[i][j.first] > dist[i][cur] + j.second) {
-                    q.push(j.first);
-                    dist[i][j.first] = dist[i][cur] + j.second;
+            for (auto &i : adj[cur]) {
+                if (dist[i] == inf) {
+                    q.push(i);
+                    dist[i] = dist[cur] + 1;
+                    d[dist[i] % 2].push_back(i + 1);
                 }
             }
         }
-    }
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            if (dist[i][j] != grid[i][j]) {
-                cout << "NO"
-                     << "\n";
-                return 0;
-            }
+        if (d[0].size() > d[1].size()) {
+            swap(d[0], d[1]);
         }
+        cout << d[0].size() << "\n";
+        print(d[0]);
     }
-    cout << "YES"
-         << "\n";
     return 0;
 }
