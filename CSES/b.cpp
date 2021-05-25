4c2ff76dd4eff994dbf5d60d1ee019fd54edc516
Ian Zhang <ianzhang0510@gmail.com>
2021-05-25 13:17:56 -0400

    Solved Codeforces Beta Round #89 (Div. 2) - E. Bertown roads - https://codeforces.com/contest/118/problem/E

--- CSES/b.cpp
+++ CSES/b.cpp
@@ -13,45 +13,31 @@ void setIO(const string &name) {
     freopen((name + ".out").c_str(), "w", stderr);
 #endif
 }
-template<class T>
-void print(T a, string sep = " ", string end = "\n") {
-    for (auto i : a) {
-        cout << i << sep;
-    }
-    cout << end;
-}
-const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 2e5 + 5;
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 1e5 + 5;
+
 vector<int> adj[maxn];
-vector<vector<int>> sol;
+vector<pair<int, int>> sol;
+int depth[maxn], dp[maxn];
+bool pos = true;
 
-int depth[maxn]; // also used as a visited array
-
-bool dfs(int c, int p, int d) {
+void dfs(int c, int p, int d) {
     depth[c] = d;
-    vector<int> nodes;
     for (auto &i : adj[c]) {
         if (i != p) {
             if (!depth[i]) {
-                if (dfs(i, c, d + 1)) {
-                    nodes.push_back(i);
-                }
+                sol.emplace_back(c, i);
+                dfs(i, c, d + 1);
+                dp[c] += dp[i];
+            } else if (depth[i] < depth[c]) {
+                ++dp[c];
+                sol.emplace_back(c, i);
             } else if (depth[i] > depth[c]) {
-                nodes.push_back(i);
+                --dp[c];
             }
-            assert(depth[i] != depth[c]);
         }
     }
-    if (nodes.size() % 2 == 0 || p == 0) {
-        for (int i = 0; i < int(nodes.size()) / 2; ++i) {
-            sol.push_back({nodes[2 * i], c, nodes[2 * i + 1]});
-        }
-        return true;
-    } else {
-        nodes.push_back(p);
-        for (int i = 0; i < int(nodes.size()) / 2; ++i) {
-            sol.push_back({nodes[2 * i], c, nodes[2 * i + 1]});
-        }
-        return false;
+    if (c != 1 && !dp[c]) {
+        pos = false;
     }
 }
 
@@ -66,14 +52,13 @@ int main() {
         adj[a].push_back(b);
         adj[b].push_back(a);
     }
-    for (int i = 1; i <= n; ++i) {
-        if (!depth[i]) {
-            dfs(i, 0, 1);
+    dfs(1, 0, 1);
+    if (pos) {
+        for (auto &i : sol) {
+            cout << i.first << " " << i.second << "\n";
         }
-    }
-    cout << sol.size() << "\n";
-    for (auto &i : sol) {
-        print(i);
+    } else {
+        cout << 0 << "\n";
     }
     return 0;
 }
