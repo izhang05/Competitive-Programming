289f7d224a7fc4e3a9764f1672452c63ce50ba08
Ian Zhang <ianzhang0510@gmail.com>
2021-06-01 14:13:30 -0400

    Solved Codeforces Round #693 (Div. 3) - G. Moving to the Capital - https://codeforces.com/contest/1472/problem/G

--- CSES/c.cpp
+++ CSES/c.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-//#define DEBUG
+#define DEBUG
 void setIO(const string &name) {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -16,81 +16,57 @@ void setIO(const string &name) {
 const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 2e5 + 5;
 
 vector<int> adj[maxn];
-int leaf[maxn];
-vector<pair<int, int>> rem, add, disconnected;
-
-bool dfs(int c, int p) {
-    vector<int> nodes;
-    bool l = true;
-    for (auto &i : adj[c]) {
-        if (i != p) {
-            if (dfs(i, c)) {
-                l = false;
-                nodes.push_back(i);
-            }
-        }
-    }
-    if (p != -1) {
-        nodes.push_back(p);
-    }
-    bool ret = true;
-    for (int i = 2; i < (int) nodes.size(); ++i) {
-        if (nodes[i] != p) {
-            rem.emplace_back(c + 1, nodes[i] + 1);
-            add.emplace_back(leaf[nodes[0]] + 1, nodes[i] + 1);
-#ifdef DEBUG
-            cout << c << " " << nodes[0] << " " << nodes[i] << " " << leaf[nodes[0]] << "\n";
-#endif
-            leaf[nodes[0]] = leaf[nodes[i]];
-        } else {
-            ret = false;
-            rem.emplace_back(c + 1, nodes[i] + 1);
-            disconnected.emplace_back(leaf[nodes[0]] + 1, leaf[nodes[1]] + 1);
-        }
-    }
-    if (l) {
-        leaf[c] = c;
-    } else {
-        leaf[c] = leaf[nodes[0]];
-#ifdef DEBUG
-        cout << c << " " << nodes[0] << " " << leaf[c] << "\n";
-#endif
-    }
-    return ret;
-}
-
 int main() {
     setIO("c");
-
     int t;
     cin >> t;
     while (t--) {
-        int n;
-        cin >> n;
+        int n, m;
+        cin >> n >> m;
+        vector<int> d(n, inf), dp(n, inf);
         for (int i = 0; i < n; ++i) {
             adj[i].clear();
         }
-        for (int i = 0; i < n - 1; ++i) {
+        for (int i = 0; i < m; ++i) {
             int a, b;
             cin >> a >> b;
             --a, --b;
             adj[a].push_back(b);
-            adj[b].push_back(a);
         }
-        dfs(0, -1);
-        disconnected.emplace_back(leaf[0] + 1, -1);
-        for (int i = 0; i < (int) disconnected.size() - 1; ++i) {
-            add.emplace_back(disconnected[i].second, disconnected[i + 1].first);
+        d[0] = 0;
+        queue<int> q;
+        q.push(0);
+        while (!q.empty()) {
+            int cur = q.front();
+            q.pop();
+            for (auto &i : adj[cur]) {
+                if (d[i] > d[cur] + 1) {
+                    q.push(i);
+                    d[i] = d[cur] + 1;
+                }
+            }
         }
-        assert(rem.size() == add.size());
-        cout << rem.size() << "\n";
-        add.emplace_back(0, 0);
-        for (int i = 0; i < int(rem.size()); ++i) {
-            cout << rem[i].first << " " << rem[i].second << " " << add[i].first << " " << add[i].second << "\n";
+        vector<pair<int, int>> nodes(n);
+        for (int i = 0; i < n; ++i) {
+            nodes[i] = {d[i], i};
         }
-        rem.clear();
-        add.clear();
-        disconnected.clear();
+        sort(nodes.begin(), nodes.end());
+        reverse(nodes.begin(), nodes.end());
+        for (int j = 0; j < n; ++j) {
+            int cur = nodes[j].second;
+            dp[cur] = d[cur];
+            for (auto &i : adj[cur]) {
+                if (d[i] > d[cur]) {
+                    dp[cur] = min(dp[cur], dp[i]);
+                } else {
+                    dp[cur] = min(dp[cur], d[i]);
+                }
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            cout << dp[i] << " ";
+        }
+        cout << "\n";
     }
     return 0;
 }
