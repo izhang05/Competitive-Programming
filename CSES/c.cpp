5320de2ffccee56ab4d6fcd76c35ea323a263b2d
Ian Zhang <ianzhang0510@gmail.com>
2021-06-06 09:34:44 -0400

    Solved Educational Codeforces Round 110 (Rated for Div. 2) - B. Array Reodering - https://codeforces.com/contest/1535/problem/B

--- CSES/c.cpp
+++ CSES/c.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-#define DEBUG
+//#define DEBUG
 void setIO(const string &name) {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -13,60 +13,35 @@ void setIO(const string &name) {
     freopen((name + ".out").c_str(), "w", stderr);
 #endif
 }
-const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 2e5 + 5;
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7;
 
-vector<int> adj[maxn];
 int main() {
     setIO("c");
+
     int t;
     cin >> t;
     while (t--) {
-        int n, m;
-        cin >> n >> m;
-        vector<int> d(n, inf), dp(n, inf);
+        int n;
+        cin >> n;
+        deque<int> a;
         for (int i = 0; i < n; ++i) {
-            adj[i].clear();
+            int b;
+            cin >> b;
+            if (b % 2 == 0) {
+                a.push_front(b);
+            } else {
+                a.push_back(b);
+            }
         }
-        for (int i = 0; i < m; ++i) {
-            int a, b;
-            cin >> a >> b;
-            --a, --b;
-            adj[a].push_back(b);
-        }
-        d[0] = 0;
-        queue<int> q;
-        q.push(0);
-        while (!q.empty()) {
-            int cur = q.front();
-            q.pop();
-            for (auto &i : adj[cur]) {
-                if (d[i] > d[cur] + 1) {
-                    q.push(i);
-                    d[i] = d[cur] + 1;
+        int sol = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (gcd(a[i], 2 * a[j]) > 1) {
+                    ++sol;
                 }
             }
         }
-        vector<pair<int, int>> nodes(n);
-        for (int i = 0; i < n; ++i) {
-            nodes[i] = {d[i], i};
-        }
-        sort(nodes.begin(), nodes.end());
-        reverse(nodes.begin(), nodes.end());
-        for (int j = 0; j < n; ++j) {
-            int cur = nodes[j].second;
-            dp[cur] = d[cur];
-            for (auto &i : adj[cur]) {
-                if (d[i] > d[cur]) {
-                    dp[cur] = min(dp[cur], dp[i]);
-                } else {
-                    dp[cur] = min(dp[cur], d[i]);
-                }
-            }
-        }
-        for (int i = 0; i < n; ++i) {
-            cout << dp[i] << " ";
-        }
-        cout << "\n";
+        cout << sol << "\n";
     }
     return 0;
 }
