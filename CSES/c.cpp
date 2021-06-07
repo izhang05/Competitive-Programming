d3a5c8393b5305c4d523e76303edadd736553679
Ian Zhang <ianzhang0510@gmail.com>
2021-06-07 15:56:09 -0400

    Solved Codeforces Round #617 (Div. 3) - E2. String Coloring (hard version) - https://codeforces.com/contest/1296/problem/E2

--- CSES/c.cpp
+++ CSES/c.cpp
@@ -13,35 +13,32 @@ void setIO(const string &name) {
     freopen((name + ".out").c_str(), "w", stderr);
 #endif
 }
-const int inf = 0x3f3f3f3f, mod = 1e9 + 7;
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 2e5 + 5;
+int dp[maxn], mxdp[26];
 
 int main() {
     setIO("c");
-
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        deque<int> a;
-        for (int i = 0; i < n; ++i) {
-            int b;
-            cin >> b;
-            if (b % 2 == 0) {
-                a.push_front(b);
-            } else {
-                a.push_back(b);
-            }
-        }
-        int sol = 0;
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                if (gcd(a[i], 2 * a[j]) > 1) {
-                    ++sol;
-                }
-            }
-        }
-        cout << sol << "\n";
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        a[i] = s[i] - 'a';
+        dp[i] = 1;
     }
+    int sol = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = a[i] + 1; j < 26; ++j) {
+            dp[i] = max(dp[i], mxdp[j] + 1);
+        }
+        mxdp[a[i]] = max(mxdp[a[i]], dp[i]);
+        sol = max(sol, dp[i]);
+    }
+    cout << sol << "\n";
+    for (int i = 0; i < n; ++i) {
+        cout << dp[i] << " ";
+    }
+    cout << "\n";
     return 0;
 }
