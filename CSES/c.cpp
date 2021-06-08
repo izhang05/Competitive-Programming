8f863c06eaa047c59d49e22fe83f2fbb75833c43
Ian Zhang <ianzhang0510@gmail.com>
2021-06-07 21:25:20 -0400

    Solved Codeforces Round #336 (Div. 1) - B. Zuma - https://codeforces.com/contest/607/problem/B

--- CSES/c.cpp
+++ CSES/c.cpp
@@ -13,38 +13,39 @@ void setIO(const string &name) {
     freopen((name + ".out").c_str(), "w", stderr);
 #endif
 }
-const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 2e5 + 5;
-int dp[maxn], mxdp[26];
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 505;
+int dp[maxn][maxn];
 
 int main() {
     setIO("c");
     int n;
     cin >> n;
-    string s;
-    cin >> s;
     vector<int> a(n);
     for (int i = 0; i < n; ++i) {
-        a[i] = s[i] - 'a';
-        dp[i] = 1;
+        for (int j = i; j < n; ++j) {
+            dp[i][j] = inf;
+        }
     }
-    int sol = 0;
     for (int i = 0; i < n; ++i) {
-        for (int j = a[i] + 1; j < 26; ++j) {
-            dp[i] = max(dp[i], mxdp[j] + 1);
-        }
-        mxdp[a[i]] = max(mxdp[a[i]], dp[i]);
-        sol = max(sol, dp[i]);
+        cin >> a[i];
+        dp[i][i] = 1;
     }
-    if (sol <= 2) {
-        cout << "YES"
-             << "\n";
-        for (int i = 0; i < n; ++i) {
-            cout << dp[i] - 1;
+    for (int i = n - 1; i >= 0; --i) {
+        for (int j = i + 1; j < n; ++j) {
+            dp[i][j] = min(dp[i][j], dp[i + 1][j] + 1);
+            for (int k = i + 2; k <= j; ++k) {
+                if (a[i] == a[k]) {
+                    dp[i][j] = min(dp[i][j], dp[i + 1][max(0, k - 1)] + dp[k + 1][j]);
+                }
+            }
+            if (a[i] == a[i + 1]) {
+                dp[i][j] = min(dp[i][j], dp[i + 2][j] + 1);
+            }
+#ifdef DEBUG
+            cout << i << " " << j << " " << dp[i][j] << "\n";
+#endif
         }
-        cout << "\n";
-    } else {
-        cout << "NO"
-             << "\n";
     }
+    cout << dp[0][n - 1] << "\n";
     return 0;
 }
