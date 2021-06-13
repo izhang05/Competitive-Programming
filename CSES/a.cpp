cab73eaa744620cb5348daa984886225c30dea59
Ian Zhang <ianzhang0510@gmail.com>
2021-06-12 22:22:26 -0400

    Solved Manthan, Codefest 17 - C. Helga Hufflepuff's Cup - https://codeforces.com/contest/855/problem/C

--- CSES/a.cpp
+++ CSES/a.cpp
@@ -13,219 +13,66 @@ void setIO(const string &name) {
     freopen((name + ".out").c_str(), "w", stderr);
 #endif
 }
-const int mod = 1e9 + 7, maxn = 5e3 + 5, maxo = 105;
-const long long inf = 1e18;
-
-string s;
-int p, m, t, ind = 0, n, node = 0;
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 1e5 + 5, maxx = 11;
+long long k, x, m, dp[maxn][maxx][3]; // [...][...][0] -> less than k, [1] -> exactly k, [2] -> greater than k
 vector<int> adj[maxn];
-long long mn[maxn][maxn], mx[maxn][maxo];
-bool pls;
 
-void dfs() {
-    char cur = s[ind++];
-    int a = -1, b, c = node;
-    if (cur == '(') {
-        a = ++node;
-        dfs();
-        cur = s[ind++];
-    }
-    if (cur == '?') {
-        b = ++node;
-        dfs();
-        assert(a != -1);
-        if (pls) {
-            for (int i = 0; i <= p; ++i) {
-                for (int j = 0; i + j <= p; ++j) {
-                    if (i + j < p) {
-                        mx[c][i + j + 1] = max(mx[c][i + j + 1], mx[a][i] + mx[b][j]);
-                        mn[c][i + j + 1] = min(mn[c][i + j + 1], mn[a][i] + mn[b][j]);
+long long dp2[maxx][3];
+void dfs(int c, int p) {
+    dp[c][0][0] = k - 1;
+    dp[c][1][1] = 1;
+    dp[c][0][2] = m - k;
+    for (auto &i : adj[c]) {
+        if (i != p) {
+            dfs(i, c);
+            memset(dp2, 0, sizeof(dp2));
+            for (int j = 0; j <= x; ++j) {
+                for (int l = 0; l + j <= x; ++l) {
+                    for (int n = 0; n < 3; ++n) {
+                        dp2[j + l][0] += dp[c][j][0] * dp[i][l][n];
+                        dp2[j + l][0] %= mod;
                     }
-                    mx[c][i + j] = max(mx[c][i + j], mx[a][i] - mn[b][j]);
-                    mn[c][i + j] = min(mn[c][i + j], mn[a][i] - mx[b][j]);
+                    dp2[j + l][1] += dp[c][j][1] * dp[i][l][0];
+                    dp2[j + l][1] %= mod;
+                    dp2[j + l][2] += dp[c][j][2] * dp[i][l][0];
+                    dp2[j + l][2] %= mod;
+                    dp2[j + l][2] += dp[c][j][2] * dp[i][l][2];
+                    dp2[j + l][2] %= mod;
                 }
             }
-        } else {
-            for (int i = 0; i <= m; ++i) {
-                for (int j = 0; i + j <= m; ++j) {
-                    if (i + j < m) {
-                        mx[c][i + j + 1] = max(mx[c][i + j + 1], mx[a][i] - mn[b][j]);
-                        mn[c][i + j + 1] = min(mn[c][i + j + 1], mn[a][i] - mx[b][j]);
-                    }
-                    mx[c][i + j] = max(mx[c][i + j], mx[a][i] + mx[b][j]);
-                    mn[c][i + j] = min(mn[c][i + j], mn[a][i] + mn[b][j]);
+            for (int j = 0; j <= x; ++j) {
+                for (int l = 0; l < 3; ++l) {
+                    dp[c][j][l] = dp2[j][l];
                 }
             }
         }
-    } else if (cur != ')') {
-        mx[c][0] = mn[c][0] = cur - '0';
-    }
-    while (ind < n && s[ind] == ')') {
-        ++ind;
     }
 }
 
 int main() {
     setIO("1");
 
-    cin >> s >> p >> m;
-    n = s.size(), t = p + m;
-    if (p <= m) {
-        pls = true;
-    } else {
-        pls = false;
+    int n;
+    cin >> n >> m;
+    for (int i = 0; i < n - 1; ++i) {
+        int a, b;
+        cin >> a >> b;
+        --a, --b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
     }
-    for (auto &i : mn) {
-        for (auto &j : i) {
-            j = inf;
-        }
-    }
-    for (auto &i : mx) {
-        for (auto &j : i) {
-            j = -inf;
-        }
-    }
-    dfs();
+    cin >> k >> x;
+    dfs(0, -1);
+    long long sol = 0;
+    for (int i = 0; i <= x; ++i) {
+        for (int j = 0; j < 3; ++j) {
 #ifdef DEBUG
-    for (int i = 0; i < (int) s.size(); ++i) {
-        for (int j = 0; j <= p; ++j) {
-            cout << i << " " << j << " " << mx[i][j] << " " << mn[i][j] << "\n";
+            cout << i << " " << j << " " << dp[0][i][j] << "\n";
+#endif
+            sol += dp[0][i][j];
+            sol %= mod;
         }
     }
-#endif
-    if (pls) {
-        cout << mx[0][p] << "\n";
-    } else {
-        cout << mx[0][m] << "\n";
-    }
+    cout << sol << "\n";
     return 0;
 }
-
-//#include <bits/stdc++.h>
-//
-//using namespace std;
-//
-//#define DEBUG
-//void setIO(const string &name) {
-//    ios_base::sync_with_stdio(false);
-//    cin.tie(nullptr);
-//    cin.exceptions(istream::failbit);
-//#ifdef LOCAL
-//    freopen((name + ".in").c_str(), "r", stdin);
-//    freopen((name + ".out").c_str(), "w", stdout);
-//    freopen((name + ".out").c_str(), "w", stderr);
-//#endif
-//}
-//const int mod = 1e9 + 7, maxn = 1000, maxo = 105;
-//const long long inf = 1e18;
-//
-//string s;
-//int p, m, t, ind = 0, n, node = 0;
-//vector<int> adj[maxn];
-//long long mn[maxn][maxn], mx[maxn][maxo];
-//
-//long long dfs() {
-//    char cur = s[ind++];
-//    if (cur >= '0' && cur <= '9') {
-//        while (ind < n && s[ind] == ')') {
-//            ++ind;
-//        }
-//        while (ind < n && s[ind] == ')') {
-//            ++ind;
-//        }
-//        return cur - '0';
-//    }
-//    if (cur == ')') {
-//        while (ind < n && s[ind] == ')') {
-//            ++ind;
-//        }
-//        return 10;
-//    }
-//    ++node;
-//    long long a = -1, b = -1, c = node, a_v = -2;
-//    if (cur == '(') {
-//        a_v = dfs();
-//        if (a_v == -1) {
-//            a = node;
-//        }
-//        cur = s[ind++];
-//    }
-//    if (cur == '?') {
-//        assert(a_v != -2);
-//        long long b_v = dfs();
-//        if (b_v == -1) {
-//            b = node;
-//        }
-//#ifdef DEBUG
-//        cout << c << " " << a << " " << b << " " << a_v << " " << b_v << "\n";
-//#endif
-//        if (a == -1) {
-//            if (b == -1) {
-//                mx[c][1] = mn[c][1] = a_v + b_v;
-//                mx[c][0] = mn[c][0] = a_v - b_v;
-//            } else {
-//                for (int j = 0; j <= p; ++j) {
-//                    if (j < p) {
-//                        mx[c][j + 1] = max(mx[c][j + 1], a_v + mx[b][j]);
-//                        mn[c][j + 1] = min(mn[c][j + 1], a_v + mn[b][j]);
-//                    }
-//                    mx[c][j] = max(mx[c][j], a_v - mn[b][j]);
-//                    mn[c][j] = min(mn[c][j], a_v - mx[b][j]);
-//                }
-//            }
-//        } else {
-//            if (b == -1) {
-//                for (int i = 0; i <= p; ++i) {
-//                    if (i < p) {
-//                        mx[c][i + 1] = max(mx[c][i + 1], mx[a][i] + b_v);
-//                        mn[c][i + 1] = min(mn[c][i + 1], mn[a][i] + b_v);
-//                    }
-//                    mx[c][i] = max(mx[c][i], mx[a][i] - b_v);
-//                    mn[c][i] = min(mn[c][i], mn[a][i] - b_v);
-//                }
-//            } else {
-//                for (int i = 0; i <= p; ++i) {
-//                    for (int j = 0; i + j <= p; ++j) {
-//                        if (i + j < p) {
-//                            mx[c][i + j + 1] = max(mx[c][i + j + 1], mx[a][i] + mx[b][j]);
-//                            mn[c][i + j + 1] = min(mn[c][i + j + 1], mn[a][i] + mn[b][j]);
-//                        }
-//                        mx[c][i + j] = max(mx[c][i + j], mx[a][i] - mn[b][j]);
-//                        mn[c][i + j] = min(mn[c][i + j], mn[a][i] - mx[b][j]);
-//                    }
-//                }
-//            }
-//        }
-//    }
-//    while (ind < n && s[ind] == ')') {
-//        ++ind;
-//    }
-//    return -1;
-//}
-//
-//int main() {
-//    setIO("1");
-//
-//    cin >> s >> p >> m;
-//    n = s.size(), t = p + m;
-//    for (auto &i : mn) {
-//        for (auto &j : i) {
-//            j = inf;
-//        }
-//    }
-//    for (auto &i : mx) {
-//        for (auto &j : i) {
-//            j = -inf;
-//        }
-//    }
-//    dfs();
-//#ifdef DEBUG
-//    for (int i = 0; i < (int) s.size(); ++i) {
-//        for (int j = 0; j <= p; ++j) {
-//            cout << i << " " << j << " " << mx[i][j] << " " << mn[i][j] << "\n";
-//        }
-//    }
-//#endif
-//    cout << mx[0][p] << "\n";
-//    return 0;
-//}
