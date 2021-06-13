cb8474af414f1b31547e35304d2cf46643ac1e55
Ian Zhang <ianzhang0510@gmail.com>
2021-06-12 23:00:47 -0400

    Solved Educational Codeforces Round 67 (Rated for Div. 2) - E. Tree Painting - https://codeforces.com/contest/1187/problem/E

--- CSES/a.cpp
+++ CSES/a.cpp
@@ -13,38 +13,29 @@ void setIO(const string &name) {
     freopen((name + ".out").c_str(), "w", stderr);
 #endif
 }
-const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 1e5 + 5, maxx = 11;
-long long k, x, m, dp[maxn][maxx][3]; // [...][...][0] -> less than k, [1] -> exactly k, [2] -> greater than k
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 2e5 + 5;
 vector<int> adj[maxn];
-
-long long dp2[maxx][3];
+long long sub[maxn], down[maxn], up[maxn];
+int n;
 void dfs(int c, int p) {
-    dp[c][0][0] = k - 1;
-    dp[c][1][1] = 1;
-    dp[c][0][2] = m - k;
+    sub[c] = 1;
     for (auto &i : adj[c]) {
         if (i != p) {
             dfs(i, c);
-            memset(dp2, 0, sizeof(dp2));
-            for (int j = 0; j <= x; ++j) {
-                for (int l = 0; l + j <= x; ++l) {
-                    for (int n = 0; n < 3; ++n) {
-                        dp2[j + l][0] += dp[c][j][0] * dp[i][l][n];
-                        dp2[j + l][0] %= mod;
-                    }
-                    dp2[j + l][1] += dp[c][j][1] * dp[i][l][0];
-                    dp2[j + l][1] %= mod;
-                    dp2[j + l][2] += dp[c][j][2] * dp[i][l][0];
-                    dp2[j + l][2] %= mod;
-                    dp2[j + l][2] += dp[c][j][2] * dp[i][l][2];
-                    dp2[j + l][2] %= mod;
-                }
-            }
-            for (int j = 0; j <= x; ++j) {
-                for (int l = 0; l < 3; ++l) {
-                    dp[c][j][l] = dp2[j][l];
-                }
-            }
+            sub[c] += sub[i];
+            down[c] += down[i];
+        }
+    }
+    down[c] += sub[c];
+}
+
+void dfs2(int c, int p) {
+    if (p != -1) {
+        up[c] = up[p] + down[p] - down[c] - sub[c] + n - sub[c];
+    }
+    for (auto &i : adj[c]) {
+        if (i != p) {
+            dfs2(i, c);
         }
     }
 }
@@ -52,8 +43,7 @@ void dfs(int c, int p) {
 int main() {
     setIO("1");
 
-    int n;
-    cin >> n >> m;
+    cin >> n;
     for (int i = 0; i < n - 1; ++i) {
         int a, b;
         cin >> a >> b;
@@ -61,17 +51,14 @@ int main() {
         adj[a].push_back(b);
         adj[b].push_back(a);
     }
-    cin >> k >> x;
     dfs(0, -1);
+    dfs2(0, -1);
     long long sol = 0;
-    for (int i = 0; i <= x; ++i) {
-        for (int j = 0; j < 3; ++j) {
+    for (int i = 0; i < n; ++i) {
+        sol = max(sol, down[i] + up[i]);
 #ifdef DEBUG
-            cout << i << " " << j << " " << dp[0][i][j] << "\n";
+        cout << i << " " << down[i] << " " << up[i] << "\n";
 #endif
-            sol += dp[0][i][j];
-            sol %= mod;
-        }
     }
     cout << sol << "\n";
     return 0;
