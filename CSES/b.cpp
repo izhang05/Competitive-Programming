e75f4ffecd9d90d90ed8d6562e92da4b906ca85d
Ian Zhang <ianzhang0510@gmail.com>
2021-06-14 23:28:33 -0400

    Solved CCC '20 S3 - Searching for Strings - https://dmoj.ca/problem/ccc20s3

--- CSES/b.cpp
+++ CSES/b.cpp
@@ -13,33 +13,55 @@ void setIO(const string &name) {
     freopen((name + ".out").c_str(), "w", stderr);
 #endif
 }
-const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 2505;
-int grid[maxn][maxn];
+const long long p = 9973, m1 = 1e9 + 9, m2 = 1e9 + 7;
+
+int freq_s[26], freq_cur[26];
 
 int main() {
-    setIO("1");
+    setIO("b");
 
-    int n;
-    cin >> n;
+    string s, t;
+    cin >> s >> t;
+    int n = (int) s.size(), m = (int) t.size();
+    if (m < n) {
+        cout << 0 << "\n";
+        return 0;
+    }
+    long long poly1 = 0, poly2 = 0, p_pow1 = 1, p_pow2 = 1;
+    set<pair<long long, long long>> sol;
     for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            cin >> grid[i][j];
-            if (i == j && grid[i][j]) {
-                cout << "NOT MAGIC"
-                     << "\n";
-                return 0;
-            }
+        ++freq_s[s[i] - 'a'], ++freq_cur[t[i] - 'a'];
+        poly1 = (poly1 * p + t[i] - 'a') % m1;
+        poly2 = (poly2 * p + t[i] - 'a') % m2;
+        p_pow1 = p_pow1 * p % m1;
+        p_pow2 = p_pow2 * p % m2;
+    }
+    bool good = true;
+    for (int j = 0; j < 26; ++j) {
+        if (freq_s[j] != freq_cur[j]) {
+            good = false;
+            break;
         }
     }
-    for (int i = 0; i < n; ++i) {
-        for (int j = i + 1; j < n; ++j) {
-            if (grid[i][j] != grid[j][i]) {
-                cout << "NOT MAGIC"
-                     << "\n";
-                return 0;
-            }
-        }
+    if (good) {
+        sol.insert({poly1, poly2});
     }
 
+    for (int i = 0; i + n < m; ++i) {
+        --freq_cur[t[i] - 'a'], ++freq_cur[t[i + n] - 'a'];
+        poly1 = (poly1 * p - p_pow1 * (t[i] - 'a') % m1 + t[i + n] - 'a' + m1) % m1;
+        poly2 = (poly2 * p - p_pow2 * (t[i] - 'a') % m2 + t[i + n] - 'a' + m2) % m2;
+        good = true;
+        for (int j = 0; j < 26; ++j) {
+            if (freq_s[j] != freq_cur[j]) {
+                good = false;
+                break;
+            }
+        }
+        if (good) {
+            sol.insert({poly1, poly2});
+        }
+    }
+    cout << sol.size() << "\n";
     return 0;
 }
