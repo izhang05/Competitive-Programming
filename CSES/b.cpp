2dea60c1e2b2e7b6dfb65694962e7be412f429a7
Ian Zhang <ianzhang0510@gmail.com>
2021-06-16 19:17:41 -0400

    Solved Mail.Ru Cup 2018 Round 3 - E. Check Transcription - https://codeforces.com/problemset/problem/1056/E

--- CSES/b.cpp
+++ CSES/b.cpp
@@ -13,44 +13,88 @@ void setIO(const string &name) {
     freopen((name + ".out").c_str(), "w", stderr);
 #endif
 }
-const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 5e3 + 5;
-long long dp[maxn][maxn], n;
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 1e6 + 5;
+const long long m1 = 1e9 + 9, p = 9973;
+
+long long pw[maxn]; // Stores the powers of p modulo m1
+int n;
+string s;
+long long hsh[maxn];
+
+void calc_hashes() {
+    hsh[0] = 1;
+    for (int i = 0; i < n; ++i) {
+        hsh[i + 1] = ((hsh[i] * p) % m1 + s[i]) % m1;
+    }
+}
+
+long long get_hash(int a, int b) { // [a, b]
+    return (hsh[b + 1] - (hsh[a] * pw[b - a + 1]) % m1 + m1) % m1;
+}
+
+void calc_pow() {
+    pw[0] = 1;
+    for (int i = 1; i < maxn; ++i) {
+        pw[i] = (pw[i - 1] * p) % m1;
+    }
+}
 
 int main() {
     setIO("b");
-    string s;
-    cin >> s;
+    string t;
+    cin >> t >> s;
     n = int(s.size());
-    for (int i = 0; i < n; ++i) {
-        dp[i][i] = 1;
-        if (i + 1 < n && s[i] == s[i + 1]) {
-            dp[i][i + 1] = 2;
+    calc_pow();
+    calc_hashes();
+    int zero = 0, one = 0;
+    for (auto &i : t) {
+        if (i == '0') {
+            ++zero;
+        } else {
+            ++one;
         }
     }
-    for (int len = 3; len <= n; ++len) {
-        for (int i = 0; i + len - 1 < n; ++i) {
-            int j = i + len - 1;
-            if (s[i] == s[j]) {
-                if (dp[i + 1][j - 1]) {
-                    dp[i][j] = min(dp[i][i + len / 2 - 1], dp[j - len / 2 + 1][j]) + 1;
-                }
+    if (zero < one) {
+        swap(zero, one);
+        for (auto &i : t) {
+            if (i == '1') {
+                i = '0';
+            } else {
+                i = '1';
             }
         }
     }
-    vector<long long> pre(n + 1);
-    for (int i = 0; i < n; ++i) {
-        for (int j = i; j < n; ++j) {
-            ++pre[0], --pre[dp[i][j]];
-#ifdef DEBUG
-            cout << i << " " << j << " " << dp[i][j] << "\n";
-#endif
+    int sol = 0;
+    for (int i = 1; i * zero < n; ++i) {
+        if ((n - i * zero) % one == 0) {
+            long long hash0 = -1, hash1 = -1;
+            int s1 = (n - i * zero) / one;
+            bool pos = true;
+            int ind = 0;
+            for (auto &j : t) {
+                if (j == '0') {
+                    if (hash0 == -1) {
+                        hash0 = get_hash(ind, ind + i - 1);
+                    } else if (get_hash(ind, ind + i - 1) != hash0) {
+                        pos = false;
+                        break;
+                    }
+                    ind += i;
+                } else {
+                    if (hash1 == -1) {
+                        hash1 = get_hash(ind, ind + s1 - 1);
+                    } else if (get_hash(ind, ind + s1 - 1) != hash1) {
+                        pos = false;
+                        break;
+                    }
+                    ind += s1;
+                }
+            }
+            if (pos && hash0 != hash1) {
+                ++sol;
+            }
         }
     }
-    long long cur = 0;
-    for (int i = 0; i < n; ++i) {
-        cur += pre[i];
-        cout << cur << " ";
-    }
-    cout << "\n";
+    cout << sol << "\n";
     return 0;
 }
