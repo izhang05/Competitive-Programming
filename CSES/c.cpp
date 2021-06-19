234928d88ea40dc0dec8e80a80ddf37503af01a7
Ian Zhang <ianzhang0510@gmail.com>
2021-06-18 23:20:51 -0400

    Solved IZhO 2014 - B. Bank - https://oj.uz/problem/view/IZhO14_bank

--- CSES/c.cpp
+++ CSES/c.cpp
@@ -1,7 +1,5 @@
 #include <bits/stdc++.h>
 
-#include <utility>
-
 using namespace std;
 
 //#define DEBUG
@@ -15,138 +13,50 @@ void setIO(const string &name) {
     freopen((name + ".out").c_str(), "w", stderr);
 #endif
 }
-const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 1e5 + 5;
-
-const long long m1 = 1e9 + 9, p = 9973;
-long long pw[maxn]; // Stores the powers of p modulo m1
-void calc_pow() {
-    pw[0] = 1;
-    for (int i = 1; i < maxn; ++i) {
-        pw[i] = (pw[i - 1] * p) % m1;
-    }
-}
-
-struct shash {
-    int n;
-    string s;
-
-    long long hsh[maxn];
-
-    void calc_hashes() {
-        hsh[0] = 1;
-        for (int i = 0; i < n; ++i) {
-            hsh[i + 1] = ((hsh[i] * p) % m1 + s[i]) % m1;
-        }
-    }
-
-    long long get_hash(int a, int b) { // [a, b]
-        return (hsh[b + 1] - (hsh[a] * pw[b - a + 1]) % m1 + m1) % m1;
-    }
-
-    void init(int a, string b) {
-        n = a;
-        s = move(b);
-        calc_hashes();
-    }
-
-    bool operator==(const shash &rhs) const {
-        return s == rhs.s;
-    }
-};
-
-bool substring(shash a, shash b) {
-    if (a.n > b.n) {
-        swap(a, b);
-    }
-    for (int k = 0; k + a.n < b.n; ++k) {
-        if (a.get_hash(0, a.n - 1) == b.get_hash(k, k + a.n - 1)) {
-            return true;
-        }
-    }
-    return false;
-}
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 21;
+int dp[1 << maxn], rem[1 << maxn];
 
 int main() {
     setIO("c");
 
-    calc_pow();
-    vector<shash> s(3);
-    vector<int> order(3);
-    int total = 0;
-    for (int i = 0; i < 3; ++i) {
-        order[i] = i;
-        string a;
-        cin >> a;
-        s[i].init(int(a.size()), a);
-        total += int(a.size());
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
     }
-    for (int i = 0; i < 3; ++i) {
-        for (int j = i + 1; j < 3; ++j) {
-            if (substring(s[i], s[j])) {
-                total -= min(s[i].n, s[j].n);
-                if (s[i].n >= s[j].n) {
-                    s.erase(find(s.begin(), s.end(), s[j]));
+    vector<int> b(m);
+    for (int i = 0; i < m; ++i) {
+        cin >> b[i];
+    }
+    memset(dp, -1, sizeof(dp));
+    for (int mask = 1; mask < (1 << m); ++mask) {
+        for (int i = 0; i < m; ++i) {
+            if (mask & (1 << i)) {
+                int pre = mask ^ (1 << i);
+                if (rem[pre] + b[i] == a[dp[pre] + 1]) {
+                    dp[mask] = dp[pre] + 1;
+                    rem[mask] = 0;
                 } else {
-                    s.erase(find(s.begin(), s.end(), s[i]));
+                    if (dp[pre] >= dp[mask]) {
+                        dp[mask] = dp[pre];
+                        rem[mask] = rem[pre] + b[i];
+                    }
                 }
-                if (substring(s[0], s[1])) {
-                    cout << max(s[0].n, s[1].n) << "\n";
+                if (dp[mask] == n - 1) {
+#ifdef DEBUG
+                    cout << mask << " " << dp[mask] << " " << rem[mask] << "\n";
+#endif
+                    cout << "YES\n";
                     return 0;
                 }
-                int dist = min(s[0].n, s[1].n);
-                int l = s[0].n - dist, r = dist - 1;
-                while (l < s[0].n && r >= 0) {
-                    if (s[0].get_hash(l, s[0].n - 1) == s[1].get_hash(0, r)) {
-                        break;
-                    }
-                    ++l, --r;
-                }
-                ++r;
-                int sol = total - r;
-
-                dist = min(s[0].n, s[1].n);
-                l = s[1].n - dist, r = dist - 1;
-                while (r >= 0) {
-                    if (s[1].get_hash(l, s[1].n - 1) == s[0].get_hash(0, r)) {
-                        break;
-                    }
-                    ++l, --r;
-                }
-                ++r;
-                cout << min(sol, total - r) << "\n";
-                return 0;
             }
         }
+#ifdef DEBUG
+        cout << mask << " " << dp[mask] << " " << rem[mask] << "\n";
+#endif
     }
-    int sol = total;
-    do {
-        vector<shash> cur(int(order.size()));
-        for (int i = 0; i < int(order.size()); ++i) {
-            cur[i] = s[order[i]];
-        }
-        int cur_sol = total;
-        int dist = min(cur[0].n, cur[1].n);
-        int l = cur[0].n - dist, r = dist - 1;
-        while (l < cur[0].n && r >= 0) {
-            if (cur[0].get_hash(l, cur[0].n - 1) == cur[1].get_hash(0, r)) {
-                break;
-            }
-            ++l, --r;
-        }
-        ++r;
-        cur_sol -= r;
-        dist = min(cur[1].n, cur[2].n);
-        l = cur[1].n - dist, r = dist - 1;
-        while (l < cur[1].n && r >= 0) {
-            if (cur[1].get_hash(l, cur[1].n - 1) == cur[2].get_hash(0, r)) {
-                break;
-            }
-            ++l, --r;
-        }
-        ++r;
-        cur_sol -= r;
-        sol = min(sol, cur_sol);
-    } while (next_permutation(order.begin(), order.end()));
-    cout << sol << "\n";
+
+    cout << "NO\n";
     return 0;
 }
