ca0106f53733e70a1c1932df52da1549cefa1290
Ian <ianzhang0510@gmail.com>
2021-06-29 19:17:12 -0400

    Solved Codeforces Round #442 (Div. 2) - E. Danil and a Part-time Job - https://codeforces.com/contest/877/problem/E

--- cf/contest/877/e/e.cpp
+++ cf/contest/877/e/e.cpp
@@ -17,42 +17,91 @@ void setIO(const string &name) {
 #endif
 }
 const int maxn = 2e5 + 5;
-bool marked[8 * maxn];
-long long t[8 * maxn];
 
-void push(int v) {
-    if (marked[v]) {
-        t[v * 2] = t[v * 2 + 1] = t[v];
-        marked[v * 2] = marked[v * 2 + 1] = true;
-        marked[v] = false;
-    }
-}
+struct segtree {
+    int size{};
+    long long neutral = 0, no_op = 0;
+    vector<long long> operations, values;
 
-void update(int v, int tl, int tr, int l, int r, int new_val) {
-    if (l > r)
-        return;
-    if (l == tl && tr == r) {
-        t[v] = new_val;
-        marked[v] = true;
-    } else {
-        push(v);
-        int tm = (tl + tr) / 2;
-        update(v * 2, tl, tm, l, min(r, tm), new_val);
-        update(v * 2 + 1, tm + 1, tr, max(l, tm + 1), r, new_val);
+    long long modify_op(long long a, long long b, long long len) {
+        if (b == no_op) {
+            return a;
+        }
+        if (b == 0) {
+            return a;
+        }
+        if (b == 1) {
+            return len - a;
+        }
+        assert(false);
     }
-}
 
-int get(int v, int tl, int tr, int pos) {
-    if (tl == tr) {
-        return t[v];
+    long long calc_op(long long a, long long b) {
+        return a + b;
     }
-    push(v);
-    int tm = (tl + tr) / 2;
-    if (pos <= tm)
-        return get(v * 2, tl, tm, pos);
-    else
-        return get(v * 2 + 1, tm + 1, tr, pos);
-}
+
+    void apply_mod_op(long long &a, long long b, long long len) {
+        a = modify_op(a, b, len);
+    }
+
+    void init(int n) {
+        size = 1;
+        while (size < n) {
+            size *= 2;
+        }
+        operations.resize(2 * size);
+        values.resize(2 * size);
+    }
+
+    void propagate(int x, int lx, int rx) {
+        if (rx - lx == 1) {
+            return;
+        }
+        int m = (lx + rx) / 2;
+        operations[2 * x + 1] ^= operations[x];
+        operations[2 * x + 2] ^= operations[x];
+        apply_mod_op(values[2 * x + 1], operations[x], m - lx);
+        apply_mod_op(values[2 * x + 2], operations[x], rx - m);
+        operations[x] = no_op;
+    }
+
+    void upd(int l, int r, int v, int x, int lx, int rx) {
+        propagate(x, lx, rx);
+        if (lx >= r || rx <= l) {
+            return;
+        }
+        if (lx >= l && rx <= r) {
+            operations[x] ^= v;
+            apply_mod_op(values[x], v, rx - lx);
+            return;
+        }
+        int m = (lx + rx) / 2;
+
+        upd(l, r, v, 2 * x + 1, lx, m), upd(l, r, v, 2 * x + 2, m, rx);
+        values[x] = calc_op(values[2 * x + 1], values[2 * x + 2]);
+    }
+
+    void upd(int l, int r, int v) {
+        upd(l, r, v, 0, 0, size);
+    }
+
+    long long query(int l, int r, int x, int lx, int rx) {
+        propagate(x, lx, rx);
+        if (lx >= r || rx <= l) {
+            return neutral;
+        }
+        if (lx >= l && rx <= r) {
+            return values[x];
+        }
+        int m = (lx + rx) / 2;
+        long long m1 = query(l, r, 2 * x + 1, lx, m), m2 = query(l, r, 2 * x + 2, m, rx);
+        return calc_op(m1, m2);
+    }
+
+    long long query(int l, int r) {
+        return query(l, r, 0, 0, size);
+    }
+};
 
 const int inf = 0x3f3f3f3f, mod = 1e9 + 7;
 vector<int> adj[maxn];
@@ -80,12 +129,28 @@ int main() {
         adj[a].push_back(i);
     }
     dfs(0, -1);
+    segtree seg;
+    seg.init(n);
     for (int i = 0; i < n; ++i) {
         int s;
         cin >> s;
-        update(0, 0, 2 * n, tin[i], tout[i] + 1, s);
+        if (s) {
+            seg.upd(tin[i], tin[i] + 1, 1);
+        }
+    }
+    int q;
+    cin >> q;
+    while (q--) {
+        string s;
+        cin >> s;
+        int x;
+        cin >> x;
+        --x;
+        if (s == "get") {
+            cout << seg.query(tin[x], tout[x] + 1) << "\n";
+        } else {
+            seg.upd(tin[x], tout[x] + 1, 1);
+        }
     }
     return 0;
 }
-
-
