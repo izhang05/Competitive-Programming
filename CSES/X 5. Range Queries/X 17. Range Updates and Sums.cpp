b832b6681d3db995ddc9beb50f99ac46c58b9fc4
Ian <ianzhang0510@gmail.com>
2021-06-30 01:13:36 -0400

    Solved CSES - Range Updates and Sums - https://cses.fi/problemset/task/1735/

--- CSES/X 5. Range Queries/X 17. Range Updates and Sums.cpp
+++ CSES/X 5. Range Queries/X 17. Range Updates and Sums.cpp
@@ -16,94 +16,117 @@ void setIO(const string &name) {
 
 const int inf = 0x3f3f3f3f, mod = 1e9 + 7;
 
-struct item {
-    long long sum, assign;
-};
-
 struct segtree {
     int size{};
-    item neutral = {0, -1};
-    vector<item> t;
+    vector<long long> val, lz_add, lz_set;
 
     void init(int n) {
         size = 1;
         while (size < n) {
             size *= 2;
         }
-        t.resize(2 * size);
+        val.resize(2 * size);
+        lz_add.resize(2 * size);
+        lz_set.resize(2 * size);
     }
 
-
-    item merge(item a, item b) {
-        if (b.assign == neutral.assign) {
-            return {a.sum + b.sum, neutral.assign};
-        }
-        return {b.assign, b.assign};
+    void pushup(int x) {
+        val[x] = val[2 * x + 1] + val[2 * x + 2];
     }
 
     void propagate(int x, int lx, int rx) {
         if (rx - lx == 1) {
             return;
         }
-        t[2 * x + 1] = merge(t[2 * x + 1], t[x]);
-        t[2 * x + 2] = merge(t[2 * x + 2], t[x]);
-        t[x] = neutral;
+        int m = (lx + rx) / 2;
+        if (lz_set[x] != 0) {
+            // range set
+            lz_set[2 * x + 1] = lz_set[2 * x + 2] = lz_set[x];
+            val[2 * x + 1] = (m - lx) * lz_set[x];
+            val[2 * x + 2] = (rx - m) * lz_set[x];
+            lz_add[2 * x + 1] = lz_add[2 * x + 2] = 0;
+            lz_set[x] = 0;
+        } else if (lz_add[x] != 0) {
+            // range add
+            if (lz_set[2 * x + 1] == 0) {
+                lz_add[2 * x + 1] += lz_add[x];
+            } else {
+                lz_set[2 * x + 1] += lz_add[x];
+                assert(lz_add[2 * x + 1] == 0);
+                lz_add[2 * x + 1] = 0;
+            }
+            if (lz_set[2 * x + 2] == 0) {
+                lz_add[2 * x + 2] += lz_add[x];
+            } else {
+                lz_set[2 * x + 2] += lz_add[x];
+                assert(lz_add[2 * x + 2] == 0);
+                lz_add[2 * x + 2] = 0;
+            }
+            val[2 * x + 1] += (m - lx) * lz_add[x];
+            val[2 * x + 2] += (rx - m) * lz_add[x];
+            lz_add[x] = 0;
+        }
     }
 
-    void add(int l, int r, int v, int x, int lx, int rx) {
-        propagate(x, lx, rx);
+    void add(int l, int r, long long v, int x, int lx, int rx) {
         if (lx >= r || rx <= l) {
             return;
         }
         if (lx >= l && rx <= r) {
-            t[x] = merge(t[x], {v, -1});
+            val[x] += (rx - lx) * v;
+            if (lz_set[x] == 0) {
+                lz_add[x] += v;
+            } else {
+                lz_set[x] += v;
+            }
             return;
         }
+        propagate(x, lx, rx);
         int m = (lx + rx) / 2;
-
         add(l, r, v, 2 * x + 1, lx, m), add(l, r, v, 2 * x + 2, m, rx);
+        pushup(x);
     }
 
-    void add(int l, int r, int v) {
+    void add(int l, int r, long long v) {
         add(l, r, v, 0, 0, size);
     }
 
-    void assign(int l, int r, int v, int x, int lx, int rx) {
-        propagate(x, lx, rx);
+    void set(int l, int r, long long v, int x, int lx, int rx) {
         if (lx >= r || rx <= l) {
             return;
         }
         if (lx >= l && rx <= r) {
-            t[x] = merge(t[x], {0, v});
+            val[x] = (rx - lx) * v;
+            lz_add[x] = 0;
+            lz_set[x] = v;
             return;
         }
-        int m = (lx + rx) / 2;
-
-        assign(l, r, v, 2 * x + 1, lx, m), assign(l, r, v, 2 * x + 2, m, rx);
-    }
-
-    void assign(int l, int r, int v) {
-        assign(l, r, v, 0, 0, size);
-    }
-
-
-    item query(int l, int r, int x, int lx, int rx) {
         propagate(x, lx, rx);
+        int m = (lx + rx) / 2;
+        set(l, r, v, 2 * x + 1, lx, m), set(l, r, v, 2 * x + 2, m, rx);
+        pushup(x);
+    }
+
+    void set(int l, int r, long long v) {
+        set(l, r, v, 0, 0, size);
+    }
+
+    long long query(int l, int r, int x, int lx, int rx) {
         if (lx >= r || rx <= l) {
-            return neutral;
+            return 0;
         }
         if (lx >= l && rx <= r) {
-            return t[x];
+            return val[x];
         }
+        propagate(x, lx, rx);
         int m = (lx + rx) / 2;
-        return merge(query(l, r, 2 * x + 1, lx, m), query(l, r, 2 * x + 2, m, rx));
+        return query(l, r, 2 * x + 1, lx, m) + query(l, r, 2 * x + 2, m, rx);
     }
 
-    item query(int l, int r) {
+    long long query(int l, int r) {
         return query(l, r, 0, 0, size);
     }
 };
-
 
 int main() {
     setIO("17");
@@ -114,25 +137,22 @@ int main() {
     for (int i = 0; i < n; ++i) {
         int a;
         cin >> a;
-        seg.assign(i, i + 1, a);
+        seg.set(i, i + 1, a);
     }
     while (q--) {
-        int t;
-        cin >> t;
+        int t, a, b;
+        cin >> t >> a >> b;
+        --a, --b;
         if (t == 1) {
-            int l, r, x;
-            cin >> l >> r >> x;
-            --l, --r;
-            seg.add(l, r + 1, x);
+            int x;
+            cin >> x;
+            seg.add(a, b + 1, x);
         } else if (t == 2) {
-            int l, r, x;
-            cin >> l >> r >> x;
-            --l, --r;
-            seg.assign(l, r + 1, x);
+            int x;
+            cin >> x;
+            seg.set(a, b + 1, x);
         } else if (t == 3) {
-            int l, r;
-            cin >> l >> r;
-            cout << seg.query(l, r + 1).sum << "\n";
+            cout << seg.query(a, b + 1) << "\n";
         }
     }
     return 0;
--- CSES/a.cpp
+++ CSES/a.cpp
@@ -1,81 +1,116 @@
+/* Author: izhang05
+ * Time: 06-29-2021 14:33:25
+**/
 #include <bits/stdc++.h>
 
 using namespace std;
 
-const int maxn = 15;
-const long double eps = 1e-3, inf = 1e18;
-long double dp[1 << maxn][maxn], p[maxn]; // p[i] = pow(m,i)
-
-struct mouse {
-    long double x, y, s;
-};
-
-long double dist(mouse a, mouse b) {
-    return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
-}
-int n;
-long double m;
-vector<mouse> a;
-mouse orig = {0, 0, 0};
-bool check(long double v) {
-    for (auto &i : dp) {
-        for (long double &j : i) {
-            j = inf;
-        }
-    }
-    for (int i = 0; i < n; ++i) {
-        long double time = dist(orig, a[i]) / v;
-        if (time <= a[i].s) {
-            dp[1 << i][i] = time;
-        }
-    }
-    for (int mask = 1; mask < (1 << n); ++mask) {
-        int num_mice = __builtin_popcount(mask) - 1;
-        for (int i = 0; i < n; ++i) {
-            if (mask & (1 << i)) {
-                int pre = mask ^ (1 << i);
-                for (int j = 0; j < n; ++j) {
-                    if (pre & (1 << j)) {
-                        long double cur_v = v * p[num_mice], time = dp[pre][j] + dist(a[j], a[i]) / cur_v;
-                        if (dp[pre][j] != inf && time <= a[i].s) {
-                            dp[mask][i] = min(dp[mask][i], time);
-                        }
-                    }
-                }
-            }
-        }
-    }
-    for (int i = 0; i < n; ++i) {
-        if (dp[(1 << n) - 1][i] != inf) {
-            return true;
-        }
-    }
-    return false;
-}
-
-int main() {
+//#define DEBUG
+void setIO(const string &name) {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cin.exceptions(istream::failbit);
-    cin >> n;
-    a.resize(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> a[i].x >> a[i].y >> a[i].s;
+}
+const int maxn = 2e5 + 5;
+
+struct segtree {
+    int size{};
+    long long neutral = 0, no_op = LLONG_MAX - 1;
+    vector<long long> operations, values;
+
+    long long modify_op(long long a, long long b, long long len) {
+        if (b == no_op) {
+            return a;
+        }
+        return b * len;
     }
-    cin >> m;
-    p[0] = 1;
-    for (int i = 1; i < maxn; ++i) {
-        p[i] = p[i - 1] * m;
+
+    long long calc_op(long long a, long long b) {
+        return a + b;
     }
-    long double l = 0, r = 1e9;
-    while (r - l > eps) {
-        long double mid = (l + r) / 2;
-        if (check(mid)) {
-            r = mid;
+
+    void apply_mod_op(long long &a, long long b, long long len) {
+        a = modify_op(a, b, len);
+    }
+
+    void init(int n) {
+        size = 1;
+        while (size < n) {
+            size *= 2;
+        }
+        operations.resize(2 * size);
+        values.resize(2 * size);
+    }
+
+    void propagate(int x, int lx, int rx) {
+        if (rx - lx == 1) {
+            return;
+        }
+        int m = (lx + rx) / 2;
+        apply_mod_op(operations[2 * x + 1], operations[x], 1);
+        apply_mod_op(values[2 * x + 1], operations[x], m - lx);
+        apply_mod_op(operations[2 * x + 2], operations[x], 1);
+        apply_mod_op(values[2 * x + 2], operations[x], rx - m);
+        operations[x] = no_op;
+    }
+
+    void upd(int l, int r, int v, int x, int lx, int rx) {
+        propagate(x, lx, rx);
+        if (lx >= r || rx <= l) {
+            return;
+        }
+        if (lx >= l && rx <= r) {
+            apply_mod_op(operations[x], v, 1);
+            apply_mod_op(values[x], v, rx - lx);
+            return;
+        }
+        int m = (lx + rx) / 2;
+
+        upd(l, r, v, 2 * x + 1, lx, m), upd(l, r, v, 2 * x + 2, m, rx);
+        values[x] = calc_op(values[2 * x + 1], values[2 * x + 2]);
+    }
+
+    void upd(int l, int r, int v) {
+        upd(l, r, v, 0, 0, size);
+    }
+
+    long long query(int l, int r, int x, int lx, int rx) {
+        propagate(x, lx, rx);
+        if (lx >= r || rx <= l) {
+            return neutral;
+        }
+        if (lx >= l && rx <= r) {
+            return values[x];
+        }
+        int m = (lx + rx) / 2;
+        long long m1 = query(l, r, 2 * x + 1, lx, m), m2 = query(l, r, 2 * x + 2, m, rx);
+        return calc_op(m1, m2);
+    }
+
+    long long query(int l, int r) {
+        return query(l, r, 0, 0, size);
+    }
+};
+
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    segtree seg;
+    seg.init(n);
+    while (m--) {
+        int t;
+        cin >> t;
+        if (t == 1) {
+            int l, r, v;
+            cin >> l >> r >> v;
+            seg.upd(l, r, v);
         } else {
-            l = mid;
+            int l, r;
+            cin >> l >> r;
+            cout << seg.query(l, r) << "\n";
         }
     }
-    cout << fixed << setprecision(16) << l << "\n";
     return 0;
 }
