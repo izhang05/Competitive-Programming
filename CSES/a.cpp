e49dde23d2e5596e930060eacf5a31995e34388a
Ian <ianzhang0510@gmail.com>
2021-08-12 15:34:02 -0400

    Solved Yosupo - Convolution (mod 1,000,000,007) - https://judge.yosupo.jp/problem/convolution_mod_1000000007

--- CSES/a.cpp
+++ CSES/a.cpp
@@ -13,212 +13,91 @@ void setIO(const string &name) {
     freopen((name + ".out").c_str(), "w", stderr);
 #endif
 }
-template<typename T>
-T mod_inv_in_range(T a, T m) {
-    // assert(0 <= a && a < m);
-    T x = a, y = m;
-    T vx = 1, vy = 0;
-    while (x) {
-        T k = y / x;
-        y %= x;
-        vy -= k * vx;
-        std::swap(x, y);
-        std::swap(vx, vy);
-    }
-    assert(y == 1);
-    return vy < 0 ? m + vy : vy;
-}
-
-template<typename T>
-T mod_inv(T a, T m) {
-    a %= m;
-    a = a < 0 ? a + m : a;
-    return mod_inv_in_range(a, m);
-}
-
-template<int MOD_>
-struct modnum {
-    static constexpr int MOD = MOD_;
-    static_assert(MOD_ > 0, "MOD must be positive");
-
-private:
-    using ll = long long;
-
-    int v;
-
-public:
-    modnum() : v(0) {}
-    modnum(ll v_) : v(int(v_ % MOD)) {
-        if (v < 0)
-            v += MOD;
-    }
-    explicit operator int() const { return v; }
-    friend std::ostream &operator<<(std::ostream &out, const modnum &n) { return out << int(n); }
-    friend std::istream &operator>>(std::istream &in, modnum &n) {
-        ll v_;
-        in >> v_;
-        n = modnum(v_);
-        return in;
-    }
-
-    friend bool operator==(const modnum &a, const modnum &b) { return a.v == b.v; }
-    friend bool operator!=(const modnum &a, const modnum &b) { return a.v != b.v; }
-
-    modnum inv() const {
-        modnum res;
-        res.v = mod_inv_in_range(v, MOD);
-        return res;
-    }
-    friend modnum inv(const modnum &m) { return m.inv(); }
-    modnum neg() const {
-        modnum res;
-        res.v = v ? MOD - v : 0;
-        return res;
-    }
-    friend modnum neg(const modnum &m) { return m.neg(); }
-
-    modnum operator-() const {
-        return neg();
-    }
-    modnum operator+() const {
-        return modnum(*this);
-    }
-
-    modnum &operator++() {
-        v++;
-        if (v == MOD)
-            v = 0;
-        return *this;
-    }
-    modnum &operator--() {
-        if (v == 0)
-            v = MOD;
-        v--;
-        return *this;
-    }
-    modnum &operator+=(const modnum &o) {
-        v -= MOD - o.v;
-        v = (v < 0) ? v + MOD : v;
-        return *this;
-    }
-    modnum &operator-=(const modnum &o) {
-        v -= o.v;
-        v = (v < 0) ? v + MOD : v;
-        return *this;
-    }
-    modnum &operator*=(const modnum &o) {
-        v = int(ll(v) * ll(o.v) % MOD);
-        return *this;
-    }
-    modnum &operator/=(const modnum &o) {
-        return *this *= o.inv();
-    }
-
-    friend modnum operator++(modnum &a, int) {
-        modnum r = a;
-        ++a;
-        return r;
-    }
-    friend modnum operator--(modnum &a, int) {
-        modnum r = a;
-        --a;
-        return r;
-    }
-    friend modnum operator+(const modnum &a, const modnum &b) { return modnum(a) += b; }
-    friend modnum operator-(const modnum &a, const modnum &b) { return modnum(a) -= b; }
-    friend modnum operator*(const modnum &a, const modnum &b) { return modnum(a) *= b; }
-    friend modnum operator/(const modnum &a, const modnum &b) { return modnum(a) /= b; }
-};
-
-template<typename T>
-T pow(T a, long long b) {
-    assert(b >= 0);
-    T r = 1;
-    while (b) {
-        if (b & 1)
-            r *= a;
-        b >>= 1;
-        a *= a;
-    }
-    return r;
-}
-
-const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 2e3 + 5;
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7;
 const long long INFL = 0x3f3f3f3f3f3f3f3f;
-modnum<mod> dp[maxn][maxn];
+
+using cd = complex<double>;
+
+void fft(vector<cd> &a) {
+    int n = a.size(), L = 31 - __builtin_clz(n);
+    static vector<complex<long double>> R(2, 1);
+    static vector<cd> rt(2, 1); // (^ 10% faster if double)
+    for (static int k = 2; k < n; k *= 2) {
+        R.resize(n);
+        rt.resize(n);
+        auto x = polar(1.0L, acos(-1.0L) / k);
+        for (int i = k; i < 2 * k; ++i) {
+            rt[i] = R[i] = i & 1 ? R[i / 2] * x : R[i / 2];
+        }
+    }
+    vector<int> rev(n);
+    for (int i = 0; i < n; ++i) {
+        rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
+    }
+    for (int i = 0; i < n; ++i) {
+        if (i < rev[i]) {
+            swap(a[i], a[rev[i]]);
+        }
+    }
+    for (int k = 1; k < n; k *= 2) {
+        for (int i = 0; i < n; i += 2 * k) {
+            for (int j = 0; j < k; ++j) {
+                // C z = rt[j+k] * a[i+j+k]; // (25% faster if hand-rolled)  /// include-line
+                auto x = (double *) &rt[j + k], y = (double *) &a[i + j + k]; /// exclude-line
+                cd z(x[0] * y[0] - x[1] * y[1], x[0] * y[1] + x[1] * y[0]);   /// exclude-line
+                a[i + j + k] = a[i + j] - z;
+                a[i + j] += z;
+            }
+        }
+    }
+}
+
+template<int M>
+vector<long long> convMod(const vector<long long> &a, const vector<long long> &b) {
+    if (a.empty() || b.empty()) {
+        return {};
+    }
+    vector<long long> res(a.size() + b.size() - 1);
+    int B = 32 - __builtin_clz((res).size()), n = 1 << B, cut = int(sqrt(M));
+    vector<cd> L(n), R(n), outs(n), outl(n);
+    for (int i = 0; i < (int) a.size(); ++i) {
+        L[i] = cd((int) a[i] / cut, (int) a[i] % cut);
+    }
+    for (int i = 0; i < (int) b.size(); ++i) {
+        R[i] = cd((int) b[i] / cut, (int) b[i] % cut);
+    }
+    fft(L), fft(R);
+    for (int i = 0; i < n; ++i) {
+        int j = -i & (n - 1);
+        outl[j] = (L[i] + conj(L[j])) * R[i] / (2.0 * n);
+        outs[j] = (L[i] - conj(L[j])) * R[i] / (2.0 * n) / 1i;
+    }
+    fft(outl), fft(outs);
+    for (int i = 0; i < (int) res.size(); ++i) {
+        auto av = (long long) lround(real(outl[i])), cv = (long long) lround(imag(outs[i]));
+        long long bv = (long long) lround((imag(outl[i]))) + (long long) lround(real(outs[i]));
+        res[i] = ((av % M * cut + bv) % M * cut + cv) % M;
+    }
+    return res;
+}
 
 int main() {
     setIO("1");
-    string s;
-    cin >> s;
-    int n = (int) s.size();
-    s.insert(s.begin(), '0');
-    int start, end;
-    cin >> start >> end;
-    if (s[1] == 'L' && end != 1) {
-        cout << 0 << "\n";
-        return 0;
+
+    int n, m;
+    cin >> n >> m;
+    vector<long long> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
     }
-    dp[1][1] = 1;
-
-    for (int i = 2; i <= n; ++i) {
-        for (int j = 1; j <= i; ++j) {
-            modnum<mod> &cur = dp[i][j];
-            if (i == n) {
-                if (i == start || i == end) {
-                    cur += dp[i - 1][j];
-                } else {
-                    cur += (j * (j - 1) + 1) * dp[i - 1][j + 1];
-                }
-            }
-            if (i == start || i == end) {
-                // add to existing component
-                if (i == min(start, end)) {
-                    if (s[i] != 'R' || i != start) {
-                        cur += j * dp[i - 1][j];
-                    }
-                } else {
-                    if (s[i] != 'R' || i != start) {
-                        cur += (j - 1) * dp[i - 1][j];
-                    }
-                }
-
-                // create new component
-                if (s[i] != 'L' || i != start) {
-                    cur += dp[i - 1][j - 1];
-                }
-            } else {
-                // Add to existing component
-                if (s[i] == 'L') {
-                    // s[i] = 'L' -> must add to the beginning of a component ( - (i > start) is because you can't add before the start position)
-                    cur += (j - (i > start)) * dp[i - 1][j];
-                } else if (s[i] == 'R') {
-                    // s[i] = 'R' -> must add to the end of a component
-                    cur += (j - (i > end)) * dp[i - 1][j];
-                } else {
-                    // s[i] = 'B' -> can add to both sides of component
-                    cur += (2 * j - (i > start) - (i > end)) * dp[i - 1][j];
-                }
-
-                // Create new component
-                if (s[i] != 'L') {
-                    cur += dp[i - 1][j - 1];
-                }
-
-                // merge two components - can't merge with 'R' in the center because you have to go left for the right most component
-                if ((s[i] == 'L' || s[i] == 'B') && j < i) {
-                    if (i < min(start, end)) {
-                        cur += j * (j + 1) * dp[i - 1][j + 1];
-                    } else if (i < max(start, end)) {
-                        cur += j * j * dp[i - 1][j + 1];
-                    } else {
-                        cur += (j * (j - 1)) * dp[i - 1][j + 1];
-                    }
-                }
-            }
-            //            cout << i << " " << j << " " << cur << "\n";
-        }
+    vector<long long> b(m);
+    for (int i = 0; i < m; ++i) {
+        cin >> b[i];
     }
-    cout << dp[n][1] << "\n";
+    auto sol = convMod<mod>(a, b);
+    for (auto &i : sol) {
+        cout << i << " ";
+    }
+    cout << "\n";
     return 0;
 }
