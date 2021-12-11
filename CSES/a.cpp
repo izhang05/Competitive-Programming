e5393943ecbcf13d9aee7fbda4d1351680a6fc85
Ian <ianzhang0510@gmail.com>
2021-12-11 14:15:44 -0500

    NOI Supperbox - https://dunjudge.me/analysis/problems/1473/

--- CSES/a.cpp
+++ CSES/a.cpp
@@ -9,78 +9,97 @@ void setIO(const string &name) {
     cin.exceptions(istream::failbit);
 #ifdef LOCAL
     freopen(("in" + name + ".txt").c_str(), "r", stdin);
+    freopen("out.txt", "w", stdout);
+    freopen("out.txt", "w", stderr);
 #endif
 }
-const int inf = 0x3f3f3f3f, maxn = 4e6 + 5, maxs = 25;
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 1e4 + 5, maxk = 2005, maxs = 14;
 const long long INFL = 0x3f3f3f3f3f3f3f3f;
-vector<int> a;
+int c[maxn], s[maxn], sol[maxn];
+int dp[maxn][maxk];
 
-long long vals[maxs][maxn], p;
+struct query {
+    int a, b, x, ind;
+};
 
-void gen(int l, int r, int depth) {
+void dnc(int l, int r, const vector<query> &que) {
     if (r - l <= 1) {
         return;
     }
     int m = (l + r) / 2;
-    vals[depth][m - 1] = a[m - 1];
+    memset(dp[m - 1], 0, sizeof(dp[m - 1]));
+    for (int i = c[m - 1]; i < maxk; ++i) {
+        dp[m - 1][i] = s[m - 1];
+    }
     for (int i = m - 2; i >= l; --i) {
-        vals[depth][i] = (vals[depth][i + 1] * a[i]) % p;
+        for (int j = 0; j < maxk; ++j) {
+            dp[i][j] = dp[i + 1][j];
+        }
+        int mx = 0;
+
+        for (int j = 0; j + c[i] < maxk; ++j) {
+            mx = max({mx, dp[i + 1][j] + s[i], dp[i][j + c[i]]});
+            dp[i][j + c[i]] = mx;
+        }
     }
-    vals[depth][m] = a[m];
+    for (int i = c[m]; i < maxk; ++i) {
+        dp[m][i] = s[m];
+    }
     for (int i = m + 1; i < r; ++i) {
-        vals[depth][i] = (vals[depth][i - 1] * a[i]) % p;
+        for (int j = 0; j < maxk; ++j) {
+            dp[i][j] = dp[i - 1][j];
+        }
+        int mx = 0;
+        for (int j = 0; j + c[i] < maxk; ++j) {
+            mx = max({mx, dp[i - 1][j] + s[i], dp[i][j + c[i]]});
+            dp[i][j + c[i]] = mx;
+        }
     }
-    gen(l, m, depth + 1);
-    gen(m, r, depth + 1);
+    vector<query> left, right;
+    for (auto &i : que) {
+        if (i.a < m && i.b >= m) {
+            for (int j = 0; j <= i.x; ++j) {
+                sol[i.ind] = max(sol[i.ind], dp[i.a][j] + dp[i.b][i.x - j]);
+            }
+        } else if (i.b < m) {
+            left.push_back(i);
+        } else {
+            right.push_back(i);
+        }
+    }
+    dnc(l, m, left), dnc(m, r, right);
 }
 
 void test_case() {
-    int n, q;
-    cin >> n >> p >> q;
-    a.resize(n);
+    int n;
+    cin >> n;
     for (int i = 0; i < n; ++i) {
-        cin >> a[i];
+        cin >> c[i] >> s[i];
+        c[i] = min(c[i], maxk - 1);
     }
-    int size = 1;
-    while (size < n) {
-        size *= 2;
-    }
-    a.resize(size, 1);
-    vector<int> b(q / 64 + 2);
-    for (int i = 0; i < q / 64 + 2; ++i) {
-        cin >> b[i];
-    }
-    gen(0, size, 0);
-    int bits = __builtin_clz(size);
-    int sol = 0, pre_l, pre_r;
+    int q;
+    cin >> q;
+    vector<query> que(q);
     for (int i = 0; i < q; ++i) {
-        int l, r;
-        if (i % 64 == 0) {
-            l = (b[i / 64] + sol) % n;
-            r = (b[i / 64 + 1] + sol) % n;
-        } else {
-            l = (pre_l + sol) % n;
-            r = (pre_r + sol) % n;
+        cin >> que[i].a >> que[i].b >> que[i].x;
+        --que[i].a, --que[i].b;
+        que[i].ind = i;
+        if (que[i].a == que[i].b) {
+            if (que[i].x >= c[que[i].a]) {
+                sol[i] = s[que[i].a];
+            }
         }
-        if (l > r) {
-            swap(l, r);
-        }
-        if (l == r) {
-            sol = (a[l] + 1) % p;
-        } else {
-            int depth = __builtin_clz(l ^ r) - bits - 1;
-            sol = (vals[depth][l] * vals[depth][r] + 1) % p;
-        }
-        pre_l = l, pre_r = r;
     }
-    cout << sol << "\n";
+    dnc(0, n, que);
+    for (int i = 0; i < q; ++i) {
+        cout << sol[i] << "\n";
+    }
 }
 
 int main() {
-    setIO("3");
+    setIO("1");
 
     int test_case_number = 1;
-    cin >> test_case_number;
     while (test_case_number--) {
         test_case();
     }
