a130c16a60816068ba014bf65b5332d5e04ded25
Ian <ianzhang0510@gmail.com>
2022-01-04 15:05:29 -0500

    Codeforces Round #630 (Div. 2) - F. Independent Set - https://codeforces.com/contest/1332/problem/F

--- CSES/a.cpp
+++ CSES/a.cpp
@@ -2,180 +2,18 @@
 
 using namespace std;
 
-//#define DEBUG
-void setIO(const string &name) {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cin.exceptions(istream::failbit);
-#ifdef LOCAL
-    freopen(("in" + name + ".txt").c_str(), "r", stdin);
-    freopen("out.txt", "w", stdout);
-    freopen("out.txt", "w", stderr);
-#endif
-}
-const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 1e5 + 5;
-const long long INFL = 0x3f3f3f3f3f3f3f3f;
-
-struct item {
-    long long mx, lz_add;
-};
-
-struct segtree {
-    int size{};
-    vector<item> t;
-    constexpr static item neutral = {-inf, 0};
-
-    void init(int n) {
-        size = 1;
-        while (size < n) {
-            size *= 2;
-        }
-        t.resize(2 * size);
-    }
-
-    static item merge(item a, item b) {
-        return {max(a.mx, b.mx), 0};
-    }
-
-    void apply(int x, long long v) {
-        t[x].mx += v;
-        t[x].lz_add += v;
-    }
-
-    void propagate(int x, int lx, int rx) {
-        if (rx - lx == 1) {
-            return;
-        }
-        apply(2 * x + 1, t[x].lz_add), apply(2 * x + 2, t[x].lz_add);
-        t[x].lz_add = 0;
-    }
-
-    void upd(int l, int r, long long v, int x, int lx, int rx) {
-        if (lx >= r || rx <= l) {
-            return;
-        }
-        if (lx >= l && rx <= r) {
-            apply(x, v);
-            return;
-        }
-        propagate(x, lx, rx);
-        int m = (lx + rx) / 2;
-        upd(l, r, v, 2 * x + 1, lx, m), upd(l, r, v, 2 * x + 2, m, rx);
-
-        t[x] = merge(t[2 * x + 1], t[2 * x + 2]);
-    }
-    void upd(int l, int r, long long v) {
-        upd(l, r, v, 0, 0, size);
-    }
-
-    void upd(int p, long long v) {
-        upd(p, p + 1, v, 0, 0, size);
-    }
-
-    item query(int l, int r, int x, int lx, int rx) {
-        propagate(x, lx, rx);
-        if (lx >= r || rx <= l) {
-            return neutral;
-        }
-        if (lx >= l && rx <= r) {
-            return t[x];
-        }
-        int m = (lx + rx) / 2;
-        return merge(query(l, r, 2 * x + 1, lx, m), query(l, r, 2 * x + 2, m, rx));
-    }
-
-    item query(int l, int r) {
-        return query(l, r, 0, 0, size);
-    }
-
-    item query(int p) {
-        return query(p, p + 1, 0, 0, size);
-    }
-};
-
-vector<int> adj[maxn];
-int st[maxn], en[maxn], t, sz[maxn], top[maxn], depth[maxn], par[maxn];
-segtree seg;
-
-void dfs1(int c, int p, int d = 0) {
-    sz[c] = 1;
-    depth[c] = d;
-    par[c] = p;
-    st[c] = t++;
-    for (auto &i : adj[c]) {
-        if (i != p) {
-            dfs1(i, c, d + 1);
-            sz[c] += sz[i];
-            if (sz[i] > sz[adj[c][0]]) {
-                swap(adj[c][0], i);
-            }
-        }
-    }
-    en[c] = t;
-}
-void dfs2(int c, int p) {
-    for (auto &i : adj[c]) {
-        if (i != p) {
-            top[i] = (i == adj[c][0] ? top[c] : i);
-            dfs2(i, c);
-        }
-    }
-}
-
-long long path(int u, int v) {
-    long long res = -inf;
-    while (top[u] != top[v]) {
-        if (depth[top[u]] < depth[top[v]]) {
-            swap(u, v);
-        }
-        res = max(res, seg.query(st[top[u]], st[u] + 1).mx);
-        u = par[top[u]];
-    }
-    if (depth[u] > depth[v]) {
-        swap(u, v);
-    }
-    res = max(res, seg.query(st[u], st[v] + 1).mx);
-    return res;
-}
-
-void test_case() {
-    int n;
-    cin >> n;
-    seg.init(n);
-    for (int i = 0; i < n - 1; ++i) {
-        int a, b;
-        cin >> a >> b;
-        --a, --b;
-        adj[a].push_back(b);
-        adj[b].push_back(a);
-    }
-    dfs1(0, -1);
-    dfs2(0, -1);
-    int q;
-    cin >> q;
-    while (q--) {
-        string s;
-        cin >> s;
-        if (s == "add") {
-            int u, v;
-            cin >> u >> v;
-            --u;
-            seg.upd(st[u], en[u], v);
-        } else {
-            int u, v;
-            cin >> u >> v;
-            --u, --v;
-            cout << path(u, v) << "\n";
-        }
-    }
+long double f(long double x, long double y) {
+    return 3 - 2 * x - 0.5 * y;
 }
 
 int main() {
-    setIO("1");
-
-    int test_case_number = 1;
-    while (test_case_number--) {
-        test_case();
+    int steps = 1000000000;
+    long double x = 0, y = 1, b = 1, size = (long double) b / steps;
+    for (int i = 0; i < steps; ++i, b += size) {
+//        cout << "x: " << x << "\ty: " << y << "\n";
+        y += f(x, y) * size;
+        x += size;
     }
+    cout << fixed << setprecision(16) << y << "\n";
     return 0;
 }
