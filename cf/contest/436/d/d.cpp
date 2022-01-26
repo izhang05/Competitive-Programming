82fa67041d0dfeccd9dee44ce7d3fb5d13c7025e
Ian <ianzhang0510@gmail.com>
2022-01-26 16:10:00 -0500

    Codeforces Round #581 (Div. 2) - E. Natasha, Sasha and the Prefix Sums - https://codeforces.com/contest/1204/problem/E

---
+++ cf/contest/436/d/d.cpp
@@ -0,0 +1,54 @@
+/* Author: izhang
+ * Time: 01-25-2022 09:17:02
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//#define DEBUG
+void setIO(const string &name) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin.exceptions(istream::failbit);
+#ifdef LOCAL
+    freopen(("in" + name + ".txt").c_str(), "r", stdin);
+    freopen("out.txt", "w", stdout);
+    freopen("out.txt", "w", stderr);
+#endif
+}
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 2e5 + 5;
+const long long INFL = 0x3f3f3f3f3f3f3f3f;
+int pre[maxn];
+
+
+void test_case() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+    vector<int> b(m);
+    for (int i = 0; i < m; ++i) {
+        cin >> b[i];
+    }
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    for (auto &i : b) {
+        ++pre[i];
+    }
+    for (int i = 1; i < maxn; ++i) {
+        pre[i] += pre[i - 1];
+    }
+
+}
+
+int main() {
+    setIO("1");
+
+    int test_case_number = 1;
+    while (test_case_number--) {
+        test_case();
+    }
+    return 0;
+}
