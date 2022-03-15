14d6ab07734caef3038f997c5eae3499056ff1bc
Ian <ianzhang0510@gmail.com>
2022-03-15 01:39:18 -0400

    Educational Codeforces Round 17 - C. Two strings - https://codeforces.com/contest/762/problem/C

--- cf/contest/762/c/c.cpp
+++ cf/contest/762/c/c.cpp
@@ -1,4 +1,4 @@
-/* Author: red_panda_fanclub
+/* Author: izhang
  * Time: 03-15-2022 01:05:51
 **/
 #include <bits/stdc++.h>
@@ -30,6 +30,55 @@ const int inf = 0x3f3f3f3f, mod = 1e9 + 7; //998244353;
 const long long INFL = 0x3f3f3f3f3f3f3f3f;
 
 void test_case() {
+    string a, b;
+    cin >> a >> b;
+    int n = int(a.size()), m = int(b.size());
+    map<int, set<int>> occ;
+    for (int i = 0; i < n; ++i) {
+        occ[a[i]].insert(i);
+    }
+    vector<int> pre(m), suf(m + 1, -1);
+    suf[m] = inf;
+    int max_suf = -1;
+    for (int i = m - 1; i >= 0; --i) {
+        auto it = occ[b[i]].lower_bound(suf[i + 1]);
+        if (it == occ[b[i]].begin()) {
+            break;
+        }
+        suf[i] = *prev(it);
+        max_suf = max(max_suf, m - i);
+    }
+    pre[0] = -1;
+    pair<int, int> sol{-1, -1};
+    for (int i = 0; i < m; ++i) {
+        auto it = occ[b[i]].upper_bound(pre[max(0, i - 1)]);
+        if (it == occ[b[i]].end()) {
+            break;
+        }
+        pre[i] = *it;
+        auto it1 = upper_bound(suf.begin(), suf.end(), pre[i]);
+        sol = max(sol, {i + suf.end() - it1, i});
+    }
+    dbg() << imie(pre) imie(suf);
+    dbg() << sol;
+    if (max_suf > sol.first) {
+        for (int i = m - max_suf; i < m; ++i) {
+            cout << b[i];
+        }
+        cout << "\n";
+        return;
+    }
+    if (sol == make_pair(-1, -1)) {
+        cout << "-\n";
+        return;
+    }
+    for (int i = 0; i <= sol.second; ++i) {
+        cout << b[i];
+    }
+    for (int i = max(sol.second + 1, int(upper_bound(suf.begin(), suf.end(), pre[sol.second]) - suf.begin())); i < m; ++i) {
+        cout << b[i];
+    }
+    cout << "\n";
 }
 
 int main() {
