32a789b9ff16b5447e2f1237d50187bcfe856492
Ian <ianzhang0510@gmail.com>
2023-01-17 10:15:42 -0500

    Codeforces Round #841 (Div. 2) and Divide by Zero 2022 - E. Graph Cost - https://codeforces.com/contest/1731/problem/E

--- cf/contest/1731/e/e.cpp
+++ cf/contest/1731/e/e.cpp
@@ -26,22 +26,59 @@ void setIO(const string &name) {
     freopen("out.txt", "w", stderr);
 #endif
 }
-const int inf = 0x3f3f3f3f, mod = 1e9 + 7; //998244353;
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7, maxn = 1e6 + 5;
 const long long INFL = 0x3f3f3f3f3f3f3f3f;
+vector<long long> phi(maxn), phi_sum(maxn);
 
 void test_case() {
     int n;
     long long m;
     cin >> n >> m;
-
+    long long sol = m, mx_cnt = maxn - 1;
+    while (m > 0 && mx_cnt > 0) {
+        int lo = 1, hi = (int) min(mx_cnt, m), res = 1, mid;
+        while (lo <= hi) {
+            mid = (lo + hi) / 2;
+            if (mid <= phi_sum[n / (mid + 1)]) {
+                res = mid;
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        long long mx = phi_sum[n / (res + 1)] / res, take = min(m / res, mx);
+        dbg() << res << " " << mx << " " << take << " " << m;
+        m -= take * res;
+        sol += take;
+        mx_cnt = res - 1;
+    }
+    if (m > 0) {
+        cout << -1 << "\n";
+        return;
+    }
+    cout << sol << "\n";
 }
 
 int main() {
     setIO("1");
-
+    for (int i = 0; i < maxn; i++) {
+        phi[i] = i;
+    }
+    phi[1] = 0;
+    for (int i = 2; i < maxn; i++) {
+        if (phi[i] == i) {
+            for (int j = i; j < maxn; j += i) {
+                phi[j] -= phi[j] / i;
+            }
+        }
+    }
+    for (int i = 1; i < maxn; ++i) {
+        phi_sum[i] = phi_sum[i - 1] + phi[i];
+    }
     int test_case_number = 1;
     cin >> test_case_number;
     while (test_case_number--) {
+        dbg() << "Starting test:" << test_case_number;
         test_case();
     }
     return 0;
