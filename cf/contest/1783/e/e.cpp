ab066e86b31b5ad7cb5355ce4bbb49259132e7d4
Ian <ianzhang0510@gmail.com>
2023-01-19 13:07:35 -0500

    Educational Codeforces Round 141 (Rated for Div. 2) - E. Game of the Year - https://codeforces.com/contest/1783/problem/E

--- cf/contest/1783/e/e.cpp
+++ cf/contest/1783/e/e.cpp
@@ -30,7 +30,43 @@ const int inf = 0x3f3f3f3f, mod = 1e9 + 7; //998244353;
 const long long INFL = 0x3f3f3f3f3f3f3f3f;
 
 void test_case() {
-
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+    vector<int> b(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> b[i];
+    }
+    vector<int> inc(n + 2);
+    for (int i = 0; i < n; ++i) {
+        if (a[i] > b[i]) {
+            ++inc[b[i] + 1], --inc[a[i] + 1];
+        }
+    }
+    for (int i = 1; i <= n + 1; ++i) {
+        inc[i] += inc[i - 1];
+    }
+    vector<int> sol;
+    for (int k = 1; k <= n; ++k) {
+        bool good = true;
+        for (int i = k; i <= n; i += k) {
+            if (inc[i + 1] != 0) {
+                good = false;
+                break;
+            }
+        }
+        if (good) {
+            sol.push_back(k);
+        }
+    }
+    cout << sol.size() << "\n";
+    for (auto &i : sol) {
+        cout << i << " ";
+    }
+    cout << "\n";
 }
 
 int main() {
