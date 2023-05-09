19c4e2d1c985ffa39899a26ab899eafa8e646ad8
Ian <ianzhang0510@gmail.com>
2023-05-08 21:32:08 -0400

    Educational Codeforces Round 47 (Rated for Div. 2) - C. Annoying Present - https://codeforces.com/contest/1009/problem/C

---
+++ cf/contest/1009/c/c.cpp
@@ -0,0 +1,60 @@
+/* Author: izhang
+ * Time: 05-08-2023 21:23:40
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#if defined LOCAL || defined DEBUG
+#include <debug.h>
+#else
+struct dbg {
+    template<class c>
+    dbg &operator<<(const c &) { return *this; }
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#endif
+
+//#define DEBUG
+void setIO(const string &name) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin.exceptions(istream::failbit);
+#ifdef LOCAL
+    freopen(("in" + name + ".txt").c_str(), "r", stdin);
+    freopen("out.txt", "w", stdout);
+    freopen("out.txt", "w", stderr);
+#endif
+}
+const int inf = 0x3f3f3f3f, mod = 1e9 + 7; //998244353;
+const long long INFL = 0x3f3f3f3f3f3f3f3f;
+
+void test_case() {
+    long long n, m;
+    cin >> n >> m;
+    long double sol = 0;
+    for (int i = 0; i < m; ++i) {
+        long double x, d;
+        cin >> x >> d;
+        sol += n * x;
+        if (d >= 0) {
+            sol += (d * n * (n - 1)) / 2;
+        } else {
+            sol += (d * ((n - 1) / 2) * (((n - 1) / 2) + 1));
+            if (n % 2 == 0) {
+                sol += d * (n / 2);
+            }
+        }
+    }
+    cout << fixed << setprecision(15) << sol / n << "\n";
+}
+
+int main() {
+    setIO("1");
+
+    int test_case_number = 1;
+    while (test_case_number--) {
+        test_case();
+    }
+    return 0;
+}
