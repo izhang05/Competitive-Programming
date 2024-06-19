9feabb4942d2f298b1794673879b3a72869e7bcc
ianzhang <ianzhang0510@gmail.com>
2024-06-19 14:10:42 -0400

    Codeforces Round 950 (Div. 3) - G. Yasya and the Mysterious Tree - https://codeforces.com/contest/1980/problem/G

--- cf/contest/1980/g/g.cpp
+++ cf/contest/1980/g/g.cpp
@@ -42,21 +42,26 @@ void dfs(int c, int p) {
 }
 
 struct trie {
-    vector<vector<int>> x;
+    vector<array<int, 2>> x, cnt;
     int t = 1;
-    vector<vector<int>> child;
+    vector<vector<int16_t>> child;
 
     void init(int n) {
         child.resize(n);
         x.resize(n);
+        cnt.resize(n);
         for (auto &i : x) {
-            i = vector<int>{-1, -1};
+            i = array<int, 2>{-1, -1};
+        }
+        for (auto &i : cnt) {
+            i = array<int, 2>{0, 0};
         }
     }
     void build(int s) {
         int v = 0;
         for (int i = 29; i >= 0; --i) {
             int cur_b = bool((1 << i) & s);
+            ++cnt[v][cur_b];
             if (x[v][cur_b] == -1) {
                 child[v].push_back(t);
                 v = x[v][cur_b] = t++;
@@ -70,16 +75,37 @@ struct trie {
         int v = 0;
         for (int i = 29; i >= 0; --i) {
             int cur_b = !bool((1 << i) & s);
-            if (x[v][cur_b] == -1) {
+            //            dbg() << v << " " << cur_b << " " << imie(x[v][cur_b]) << " " << imie(cnt[v][cur_b])
+            //                  << " " << x[v][!cur_b] << " " << cnt[v][!cur_b];
+            if (cnt[v][cur_b] == 0) {
                 v = x[v][!cur_b];
+                assert(v != -1);
             } else {
-                    res += (1 << i);
-//                if (cur_b) {
-//                }
+                res += (1 << i);
+                //                if (cur_b) {
+                //                }
                 v = x[v][cur_b];
             }
         }
-        return res^s;
+        return res ^ s;
+    }
+    void del(int s) {
+        int v = 0;
+        for (int i = 29; i >= 0; --i) {
+            int cur_b = bool((1 << i) & s);
+            --cnt[v][cur_b];
+            //            if (x[v][cur_b] == -1) {
+            //                child[v].push_back(t);
+            //                v = x[v][cur_b] = t++;
+            //            } else {
+            v = x[v][cur_b];
+            //            }
+        }
+    }
+    void clear() {
+        x.clear();
+        child.clear();
+        cnt.clear();
     }
 };
 
@@ -100,11 +126,14 @@ void test_case() {
     }
     d[0] = depth[0] = 0;
     dfs(0, -1);
+    int even_cnt = 0, odd_cnt = 0;
     for (int i = 0; i < n; ++i) {
         if (depth[i] % 2 == 0) {
             even.build(d[i]);
+            ++even_cnt;
         } else {
             odd.build(d[i]);
+            ++odd_cnt;
         }
     }
     int tot_w = 0;
@@ -122,17 +151,35 @@ void test_case() {
             int val = d[v];
             if (depth[v] % 2 == 1) {
                 val ^= tot_w;
+                odd.del(d[v]);
+                --odd_cnt;
+            } else {
+                even.del(d[v]);
+                --even_cnt;
             }
-            int res = even.query(val ^ w) ^ val ^ w;
-            dbg() << imie(val ^ w) << " " << even.query(val ^ w);
-            dbg() << res;
-            int cur_odd = odd.query(val ^ w ^ tot_w) ^ val ^ w ^ tot_w;
-            dbg() << cur_odd;
-            cout << max(res, cur_odd) << " ";
+            int res = 0;
+            if (even_cnt) {
+                res = max(res, even.query(val ^ w) ^ val ^ w);
+            }
+            //            dbg() << imie(val ^ w) << " " << even.query(val ^ w);
+            //            dbg() << res;
+
+            if (odd_cnt) {
+                res = max(res, odd.query(val ^ w ^ tot_w) ^ val ^ w ^ tot_w);
+            }
+            cout << res << " ";
+            if (depth[v] % 2 == 1) {
+                odd.build(d[v]);
+                ++odd_cnt;
+            } else {
+                even.build(d[v]);
+                ++even_cnt;
+            }
             //            int cur = tot_w ^ w;
             //            cout << (query(d[v] ^ tot_w ^ w) ^) << " ";
         }
     }
+    even.clear(), odd.clear();
     cout << "\n";
 }
 
